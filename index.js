786ba1b6b684a814b2d4fa3d9b34369b96f433d3
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-02 00:26:19 +0100

    make it better

--- index.js
+++ index.js
@@ -14,6 +14,24 @@ client.on("ready", () => {
     client.user.setActivity(`Quido je láska ❤️`);
 });
 
+// Initialize the invite cache
+const invites = {};
+
+// A pretty useful method to create a delay without blocking the whole script.
+const wait = require('util').promisify(setTimeout);
+
+client.on('ready', () => {
+  // "ready" isn't really ready. We need to wait a spell.
+  wait(1000);
+
+  // Load all invites for all guilds and save them to the cache.
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
   // To compare, we need to load the current invite list.
