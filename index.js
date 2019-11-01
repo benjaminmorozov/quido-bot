621a7e3676fa8cdda587be1cc7ea97c51eb6797b
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-01 23:58:16 +0100

    make it better

--- index.js
+++ index.js
@@ -14,6 +14,31 @@ client.on("ready", () => {
     client.user.setActivity(`Quido je láska ❤️`);
 });
 
+client.on('guildMemberAdd', member => {
+  client.on('message', 
+  
+  
+  var role = member.guild.roles.find('name', 'Beginner role name'); // Variable to get channel ID
+  member.addRole(role); // Adds the default role to members
+  
+  member.guild.channels.get('JOIN/LEAVE Channel ID').send({embed: {
+  color: 3447003,
+  title: "**SERVER NAME** Welcome Bot!",
+  url: "WEBSITE URL",
+  description: "Welcome *" + member + "* to the **Server name** discord server!",
+  fields: [{
+      name: "Information",
+      value: "Some info on the server"
+    }
+  ],
+  timestamp: new Date(),
+  footer: {
+    icon_url: client.user.avatarURL,
+    text: "© NAME OF SERVER 2018 - 2019"
+  }
+  }})
+});
+
 client.on("message", async message => {
   // It's good practice to ignore other bots. This also makes your bot ignore itself
   // and not get into a spam loop (we call that "botception").
@@ -44,7 +69,7 @@ client.on("message", async message => {
     message.channel.send({embed: {
         color: 3447003,
         author: {
-          name: '12 Commands:',
+          name: '13 Commands:',
         },
         thumbnail: {
             "url": "http://giphygifs.s3.amazonaws.com/media/MF1kR4YmC2Z20/giphy.gif"
@@ -59,7 +84,7 @@ client.on("message", async message => {
           },
           {
             name: "Utils:",
-            value: "$avatar - Displays an avatar of the mentioned user/you.\n$help - Show all the commands.\n$server - Informations about the server.\n$user-info - Displays informations about you.",
+            value: "$avatar - Displays an avatar of the mentioned user/you.\n$help - Show all the commands.\n$report - Report an user to the server admins.\n$server - Informations about the server.\n$user-info - Displays informations about you.",
             inline: "true"
           }
         ],
