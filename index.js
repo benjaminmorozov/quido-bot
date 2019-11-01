00a0a15687fc4e998dc997e6b66bcaa9e5ece433
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-01 11:30:49 +0100

    make it better

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ client.on("message", async message => {
       return message.reply("Sorry, you don't have permissions to use this!");
     
     const activity = args.join(" ");
-    client.user.setActivity(activity, { type: 'WATCHING' });
+    client.user.setActivity(activity);
     message.channel.send(`My activity has been set to "${activity}".`).then(sentMessage => {
       sentMessage.delete(5000);
     })
