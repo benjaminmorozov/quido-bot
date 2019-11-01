9a0633a8d28cf508daf87ff17dae27b7b780ee19
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-02 00:22:06 +0100

    make it better

--- index.js
+++ index.js
@@ -14,9 +14,25 @@ client.on("ready", () => {
     client.user.setActivity(`Quido je láska ❤️`);
 });
 
+// Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
-  member.guild.channels.get('631083427936075789').send(`Welcome ${member.user.tag} to this server.`); 
+  // To compare, we need to load the current invite list.
+  member.guild.fetchInvites().then(guildInvites => {
+    // This is the *existing* invites for the guild.
+    const ei = invites[member.guild.id];
+    // Update the cached invites for the guild.
+    invites[member.guild.id] = guildInvites;
+    // Look through the invites, find the one for which the uses went up.
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    // This is just to simplify the message being sent below (inviter doesn't have a tag property)
+    const inviter = client.users.get(invite.inviter.id);
+    // Get the log channel (change to your liking)
+    const logChannel = member.guild.channels.find(channel => channel.name === "🎫welcome");
+    // A real basic message with the information we need. 
+    logChannel.send(`${member.user.tag} joined using invite code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
+  });
 });
+
 
 client.on("message", async message => {
   // It's good practice to ignore other bots. This also makes your bot ignore itself
