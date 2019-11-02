8b1d443e05b1c89166538474761ef2dde5069c36
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-02 17:48:07 +0100

    make it better

---
+++ commands/say.js
@@ -0,0 +1,11 @@
+exports.run = (client, message, [say]) => {
+    if(!message.member.roles.some(r=>["🔱OWNER🔱","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
+    return message.reply("Sorry, you don't have permissions to use this!");
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send(sayMessage);
+  };
