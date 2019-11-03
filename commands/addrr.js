f7a0a9e3022fdbe88fd5498799c43f91cb7ae77b
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-03 14:22:05 +0100

    make it better

--- commands/addrr.js
+++ commands/addrr.js
@@ -1,4 +1,9 @@
 exports.run = async (client, message, args) => {
+
+    let.msg = await message.channel.send("test");
+    await msg.react("😦");
+    await msg.react("🍆");
+
     const reactions = await message.awaitReactions(reaction => {
         return reaction.emoji.name === "😂"; 
     }, {time: 7000});
