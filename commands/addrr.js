dafbb947fda19c4ed49fc89c0ec819176d69f0aa
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-03 14:02:10 +0100

    make it better

---
+++ commands/addrr.js
@@ -0,0 +1,36 @@
+exports.run = (client, message, args) => {
+    var emojiname = ["minecraft"];
+    var rolename=["Minecraft"];
+
+    for(let n in emojiname){
+        var emoji =[msg.guild.emojis.find(r => r.name == emojiname[n])];
+        for(let i in emoji){
+            msg.react(emoji[i]);
+        }
+    }
+
+    client.on("messageReactionAdd",(reaction,user)=>{
+        if(!user) return;
+        if(user.bot)return;
+        if(!reaction.message.channel.guild) return;
+        for(let n in emojiname){
+            if(reaction.emoji.name == emojiname[n]){
+                let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);          
+                reaction.message.guild.member(user).addRole(role).catch(console.error);
+            }
+        }
+    });
+      
+      
+    client.on("messageReactionRemove",(reaction,user)=>{
+        if(!user) return;
+        if(user.bot)return;
+        if(!reaction.message.channel.guild) return;
+        for(let n in emojiname){
+            if(reaction.emoji.name == emojiname[n]){
+                let role = reaction.message.guild.roles.find(r => r.name == rolename[n]);   
+                reaction.message.guild.member(user).removeRole(role).catch(console.error);
+            }
+        }
+    });
+};
