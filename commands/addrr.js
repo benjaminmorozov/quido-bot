7ec16a0d38fd3d206016829fb1fe341c5860bb30
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-03 14:17:18 +0100

    make it better

--- commands/addrr.js
+++ commands/addrr.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args) => {
     const reactions = await message.awaitReactions(reaction => {
         return reaction.emoji.name === ":minecraft:630379820030885898"; 
-        {, {time: 7000});
+    }, {time: 7000});
     console.log(reactions);
 };
