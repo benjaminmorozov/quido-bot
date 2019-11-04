b479dbb4254b955089a223224282b56d76dc966c
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 22:06:50 +0100

    make it better

--- commands/addrr.js
+++ commands/addrr.js
@@ -15,7 +15,7 @@ exports.run = async (client, message, args) => {
     message.channel.send({embed: {
         color: 0xff5353,
         author: {
-          name: `<:counterstrike:630379893422555147>`,
+          name: `<:630379893422555147:>`,
         },
         footer: {
           icon_url: 'https://cdn.discordapp.com/icons/610434388777369602/08a037cb16972aa3cd069a055d63ca43.webp',
