d682491776d9b41d0f18461166c932b5bd248220
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 21:27:57 +0100

    make it better

--- commands/clearwarns.js
+++ commands/clearwarns.js
@@ -19,8 +19,6 @@ exports.run = async (client, message, args) => {
     };
 
     warns[member.id].warns++;
-
-    var warns = warns[member.id].warns - 1;
     
     fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
         if (err) console.log(err)
