52ef277e303641eadb9795e6f7a98a56442777dc
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 21:48:38 +0100

    make it better

--- commands/addrr.js
+++ commands/addrr.js
@@ -1,9 +1,23 @@
-exports.run = async (client, message, args) => {
-    await message.react("😦");
-    await message.react("🍆");
+const Discord = require("discord.js");
 
-    const reactions = await message.awaitReactions(reaction => {
-        return reaction.emoji.name === "😂"; 
-    }, {time: 7000});
-    console.log(reactions);
+exports.run = async (client, message, args) => {
+
+    await message.delete().catch(O_o=>{});
+
+    const a = message.guild.roles.get('630436740854841357'); // Minecraft role
+    const b = message.guild.roles.get('630436083129385002'); // Counter-Strike role
+
+    const filter = (reaction, user) => ['A','B'].includes(reaction.emoji.name) && user.id === message.author.id;
+
+    const embed = new RichEmbed()
+        .setTitle('REACH EMOJI TO GET ROLE')
+        .setDescription(`
+        
+        A
+        B
+        
+        `)
+        .setColor(0xff5353)
+        .setFooter(`ID: ${message.author.id}`); 
+    message.channel.send(embed);
 };
