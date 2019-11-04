de9d32ae6d987e2745a1c7defe95a66bce9855ae
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 18:17:01 +0100

    make it better

--- commands/warns.js
+++ commands/warns.js
@@ -10,9 +10,7 @@ exports.run = async (client, message, args) => {
     // Let's first check if we have a member and if we can kick them!
     // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
     // We can also support getting the member by ID, which would be args[0]
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
-    let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-    if(!wUser) return message.reply("Couldn't find them yo");
+    let wUser = message.mentions.members.first();
     let warnlevel = warns[wUser.id].warns;
 
     if(!warns[wUser.id]) warns[wUser.id] = {
