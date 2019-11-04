ddac62387ee1a484436c22696b6f7f8d27db1dab
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 20:04:06 +0100

    make it better

--- commands/warns.js
+++ commands/warns.js
@@ -10,9 +10,21 @@ exports.run = (client, message, args) => {
     // Let's first check if we have a member and if we can kick them!
     // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
     // We can also support getting the member by ID, which would be args[0]
+    let reason = args.join(" ").slice(22);
     let member = message.mentions.members.first();
     if(!member)
         return message.reply("please mention a valid member of this server.");
+    if(!warns[member.id]) warns[member.id] = {
+        warns: 0
+    };
 
-    message.reply(`<@${member.id}> has ${warns[member.id]} warnings.`);
+    warns[member.id].warns++;
+    
+    fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+        if (err) console.log(err)
+    });
+  
+    let warnchannel = client.channels.get(`630403969616707594`);
+
+    message.reply(`<@${member.id}> has ${warns[member.id].warns} warnings.`);
 };
