e507b4c2b5805bd0e590098f8ffece0bb2cbd378
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 19:53:34 +0100

    make it better

--- commands/warns.js
+++ commands/warns.js
@@ -10,18 +10,9 @@ exports.run = async (client, message, args) => {
     // Let's first check if we have a member and if we can kick them!
     // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
     // We can also support getting the member by ID, which would be args[0]
-    let reason = args.join(" ").slice(22);
     let member = message.mentions.members.first();
     if(!member)
         return message.reply("please mention a valid member of this server.");
-    if(!member.kickable) 
-        return message.channel.send(`Sorry ${message.author}, you cannot kick this user.`);
-    
-    if(!reason) reason = "No reason provided.";
-
-    if(!warns[member.id]) warns[member.id] = {
-        warns: 0
-    };
 
     message.reply(`<@${member.id}> has ${warns[member.id].warns} warnings.`);
 };
