9b1e49fc21ad289ab8da573edd35e39d4bdc24e7
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 15:16:58 +0100

    make it better

--- commands/newhelp.js
+++ commands/newhelp.js
@@ -2,22 +2,24 @@ exports.run = (client, message) => {
     message.channel.send({embed: {
         color: 3447003,
         author: {
-          name: '13 Commands:',
+          name: '🔰 List of all commands:',
         },
         thumbnail: {
             "url": "http://giphygifs.s3.amazonaws.com/media/MF1kR4YmC2Z20/giphy.gif"
           },
         fields: [{
           name: "Fun:",
-          value: `__${client.config.prefix}cat__ - Get an instant picture of a cute cat. (usage: ${client.config.prefix}cat)\n__${client.config.prefix}dog__ - Get an instant picture of a cute dog. (usage: ${client.config.prefix}dog)\n__${client.config.prefix}8ball__ - Ask a question to the mystic 8ball. (usage: ${client.config.prefix}8ball <question>)`
+          value: `__${client.config.prefix}cat__ - Get an instant picture of a cute cat. (usage: ${client.config.prefix}cat)\n__${client.config.prefix}dog__ - Get an instant picture of a cute dog. (usage: ${client.config.prefix}dog)\n__${client.config.prefix}8ball__ - Ask a question to the mystic 8ball. (usage: ${client.config.prefix}8ball <question>)`,
+          inline: "true"
           },
           {
             name: "Mod:",
-            value: `__${client.config.prefix}ban__ - Bans an user. (usage: ${client.config.prefix}ban <@user> <reason>)\n__${client.config.prefix}kick__ - Kicks an user. (usage: ${client.config.prefix}kick <@user> <reason>)\n__${client.config.prefix}mute__ - Mutes an user. (usage: ${client.config.prefix}mute <@user> <reason>\n__${client.config.prefix}purge__ - Deletes a custom amount of messages. (usage: ${client.config.prefix}purge <amount>)\n__${client.config.prefix}unmute__ - Unmutes an user. (usage: ${client.config.prefix}unmute <@user>)`
+            value: `__${client.config.prefix}ban__ - Bans an user. (usage: ${client.config.prefix}ban <@user> <reason>)\n__${client.config.prefix}kick__ - Kicks an user. (usage: ${client.config.prefix}kick <@user> <reason>)\n__${client.config.prefix}mute__ - Mutes an user. (usage: ${client.config.prefix}mute <@user> <reason>\n__${client.config.prefix}purge__ - Deletes a custom amount of messages. (usage: ${client.config.prefix}purge <amount>)\n__${client.config.prefix}unmute__ - Unmutes an user. (usage: ${client.config.prefix}unmute <@user>)`,
+            inline: "true"
           },
           {
-            name: "Utils:",
-            value: "$avatar - Displays an avatar of the mentioned user/you.\n$help - Show all the commands.\n$report - Report an user to the server admins.\n$server - Informations about the server.\n$user-info - Displays informations about you.",
+            name: "Misc:",
+            value: "```CSS $avatar - Displays an avatar of the mentioned user/you.\n$help - Show all the commands.\n$report - Report an user to the server admins.\n$server - Informations about the server.\n$user-info - Displays informations about you.```",
             inline: "true"
           }
         ],
