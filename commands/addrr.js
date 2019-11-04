1a19e67081ffd095283d3b725d1cf73ea6301292
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 21:59:51 +0100

    make it better

--- commands/addrr.js
+++ commands/addrr.js
@@ -2,6 +2,9 @@ const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
 
+    const minecraftemoji = client.emojis.get("630379820030885898");
+    const counterstrikeemoji = client.emojis.get("630379893422555147");
+
     await message.delete().catch(O_o=>{});
 
     const a = message.guild.roles.get('630436740854841357'); // Minecraft role
@@ -13,7 +16,12 @@ exports.run = async (client, message, args) => {
         color: 0xff5353,
         title: 'REACH EMOJI TO GET ROLE:',
         },
-        description: `:630379820030885898: :counterstrike`,
+        fields: [
+            {
+                name: `${minecraftemoji}\n${counterstrikeemoji}`,
+                value: '',
+            }
+        ],
         footer: {
           icon_url: 'https://images.discordapp.net/avatars/220644154177355777/bd6b28005c26d079486063a4976dfb44.png',
           text: "If you find a bug, please report it to our staff. ❤️"
