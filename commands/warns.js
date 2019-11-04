076f3bdcf5e3420e2871ef909544ffb922920eb6
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 19:42:55 +0100

    make it better

--- commands/warns.js
+++ commands/warns.js
@@ -3,14 +3,28 @@ const fs = require("fs");
 const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
-exports.run = (client, message, args) => {
-    if(!message.member.roles.some(r=>["🔱OWNER🔱","Administrator", "Moderator","Head Admin","Admin"].includes(r.name)) )
-    return message.reply("sorry, you don't have enough permissions to use this command!");
-    // Let's first check if we have a member and if we can kick them!
-    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
-    // We can also support getting the member by ID, which would be args[0]
-    let member = message.mentions.members.first();
-    warns[member.id].warns++;
-    
-    message.reply(`<@${member.id}> has ${warns[member.id].warns} warnings.`);
+exports.run = async(client, message, args) => {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return
+    message.reply("You don't have permssion to use this command");
+    let wUser = message.guild.member(message.mentions.users.first()) ||
+      message.guild.members.get(args[0])
+    if (!wUser) return message.reply("Couldn't find that user");
+    if (warns[wUser.id] != undefined) {
+        warnLevel = warns[wUser.id].warns
+    }
+    else {
+        message.delete().catch();
+  
+        let warnembed = new Discord.RichEmbed()
+          .setTitle("**warns**")
+          .setColor("#0xff80ff")
+          .addField("User Warns", warnlevel, true);
+      
+        message.channel.send(warnembed);
+    }
+  
+    if (!warns[wUser.id]) warns[wUser.id] = {
+      warns: 0
+    };
+  
 };
