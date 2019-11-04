d5c222d913ab289d3259368cd88802d8dd837360
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 14:37:35 +0100

    make it better

---
+++ commands/setstatus.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args) => {
+    if(!message.member.roles.some(r=>["🔱OWNER🔱","Administrator", "Moderator","Head Admin","Admin"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    const activity = args.join(" ");
+    client.user.setActivity(activity);
+    message.channel.send(`My activity has been set to "${activity}".`).then(sentMessage => {
+      sentMessage.delete(5000);
+    });
+};
