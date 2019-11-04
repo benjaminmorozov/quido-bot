8e8861dd80c6ee9701847f397aa5e8a532f33a96
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 19:35:17 +0100

    make it better

--- commands/warns.js
+++ commands/warns.js
@@ -6,12 +6,11 @@ let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 exports.run = (client, message, args) => {
     if(!message.member.roles.some(r=>["🔱OWNER🔱","Administrator", "Moderator","Head Admin","Admin"].includes(r.name)) )
     return message.reply("sorry, you don't have enough permissions to use this command!");
-
-    warns[member.id].warns++;
     // Let's first check if we have a member and if we can kick them!
     // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
     // We can also support getting the member by ID, which would be args[0]
     let member = message.mentions.members.first();
+    warns[member.id].warns++;
     
     message.reply(`<@${member.id}> has ${warns[member.id].warns} warnings.`);
 };
