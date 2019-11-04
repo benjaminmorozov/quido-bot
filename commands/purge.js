80a309fed83f2f5f91fcab46a9360d8df11997b3
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 17:51:16 +0100

    make it better

---
+++ commands/purge.js
@@ -0,0 +1,22 @@
+exports.run = (client, message, args) => {
+    if(!message.member.roles.some(r=>["🔱OWNER🔱","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
+    return message.reply("sorry, you don't have enough permissions to use this!");
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    if (!args.length) {
+        return;
+    }
+    // get the delete count, as an actual number.
+    const deleteCount = parseInt(args[0], 10);
+    
+    // Ooooh nice, combined conditions. <3
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    // So we get our messages, and delete them. Simple enough, right?
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`couldn't delete messages because of: ${error}`));
+    message.channel.send(`Deleted ${deleteCount} messages!`).then(sentMessage => {
+        sentMessage.delete(5000);
+};
