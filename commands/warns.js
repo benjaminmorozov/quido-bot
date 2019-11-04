304cba7bc83c3e8cc4061bb3e6c4aefeed6f8963
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-04 19:30:22 +0100

    make it better

--- commands/warns.js
+++ commands/warns.js
@@ -11,8 +11,7 @@ exports.run = async (client, message, args) => {
     // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
     // We can also support getting the member by ID, which would be args[0]
     let member = message.mentions.members.first();
-    let warnlevel = warns[member.id];
     
-    message.reply(`<@${member.id}> has ${warnlevel} warnings.`);
+    message.reply(`<@${member.id}> has ${warns[member.id]} warnings.`);
     
 };
