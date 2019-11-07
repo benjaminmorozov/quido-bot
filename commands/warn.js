b71c6e9c33dedb625142c15ffdba0c217c644df7
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-07 21:20:58 +0100

    test

--- commands/warn.js
+++ commands/warn.js
@@ -88,52 +88,6 @@ exports.run = async (client, message, args) => {
               });
           };
       };
-      let MemberRole = message.guild.roles.find("name", "Member");
-      if(member.roles.has(MemberRole.id)){
-        let member = message.mentions.members.first();
-        let warnchannel = client.channels.get(`630403969616707594`);
-        warns[member.id].warns++;
-
-        fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
-            if (err) console.log(err)
-            warnchannel.send({embed: {
-                color: 0xff5353,
-                author: {
-                  name: 'Warning:',
-                },
-                fields: [{
-                  name: "Warned User:",
-                  value: `<@${member.id}>`,
-                  inline: "true"
-                  },
-                  {
-                    name: "Warned In:",
-                    value: `${message.channel}`,
-                    inline: "true"
-                  },
-                  {
-                    name: "Number of Warnings:",
-                    value: `${warns[member.id].warns}`,
-                    inline: "true"
-                  },
-                  {
-                    name: "Reason:",
-                    value: `${reason}`,
-                    inline: "true"
-                  }
-                ],
-                footer: {
-                  icon_url: 'https://images.discordapp.net/avatars/220644154177355777/bd6b28005c26d079486063a4976dfb44.png',
-                  text: "If you find a bug, please report it to our staff. ❤️"
-                }
-              }
-            });
-        });
-      } else {
-      if(!member.kickable)
-          return message.channel.send(`Sorry ${message.author}, you cannot warn this user.`);
-      let warnchannel = client.channels.get(`630403969616707594`);
-    }
 
     if(warns[member.id].warns == 3){
         let muterole = message.guild.roles.find(role => role.name == "Muted");
