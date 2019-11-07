7a172e3c69734a75f79ebe30e91f92be5b70ebb2
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-07 22:23:06 +0100

    test

--- commands/newhelp.js
+++ commands/newhelp.js
@@ -3,9 +3,9 @@ exports.run = (client, message) => {
   const exampleEmbed = new Discord.RichEmbed()
   	.setColor('0xff5353')
   	.setTitle('🔰 List of all commands:')
+    .setDescription('Use the prefix: q!')
   	.setThumbnail('http://giphygifs.s3.amazonaws.com/media/MF1kR4YmC2Z20/giphy.gif')
-  	.addField('Fun', 'Some value here')
-  	.addBlankField()
+  	.addField('Fun', 'q!', true)
   	.addField('Misc', 'Some value here', true)
   	.addField('Mod', 'Some value here', true)
   	.setFooter('Thanks for being a part of our community. ❤️', 'https://images.discordapp.net/avatars/220644154177355777/bd6b28005c26d079486063a4976dfb44.png');
