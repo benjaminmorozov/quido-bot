dbb8df9a4c3833f3d05d6eebc3843f6d8780f35e
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-08 20:54:46 +0100

    bye

--- commands/leaveserverp0.js
+++
@@ -1,7 +0,0 @@
-exports.run = (client, message, args) => {
-    if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
-    return message.reply("sorry, you don't have enough permissions to use this!");
-
-    client.guilds.get(args.join(" ")).leave()
-    return message.reply("takzvaná sabotáž xddd");
-};
