032bcae2f23b9dde53afaeaaea977fba6812acf8
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-08 21:11:55 +0100

    npm snekfetch

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,9 +1,10 @@
 const snekfetch = require('snekfetch');
 
-exports.run = async (client, message, args) => {
+exports.run = (client, message, args) => {
   const { body } = await snekfetch
-      .get('https://www.reddit.com/r/hentai/hot.json')
+      .get('https://www.reddit.com/r/dankmemes.json?sort=top&t=week')
       .query({ limit: 800 });
+  const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
   const randomnumber = Math.floor(Math.random() * allowed.length)
   const embed = new Discord.RichEmbed()
   .setColor(0x00A2E8)
