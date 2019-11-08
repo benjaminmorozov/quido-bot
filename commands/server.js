ada03e3d5327d37e3992c52ba98e2fb0efb37bce
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-08 15:08:02 +0100

    test

--- commands/server.js
+++ commands/server.js
@@ -15,7 +15,7 @@ exports.run = (client, message) => {
             inline: "true"
             },
             {
-            name: "Server prefix:",
+            name: "Bot Prefix:",
             value: `**q!**`,
             inline: "true"
             },
@@ -27,10 +27,10 @@ exports.run = (client, message) => {
             {
             name: "Staff List:",
             value: "<#638804758240559154>",
-            inline: "true"
+            inline: "true"cmd
             },
             {
-            name: "Creation Date:",
+            name: "Server Creation Date:",
             value: `${message.channel.guild.createdAt.toUTCString().substr(0, 16)}`,
             inline: "true"
             },
