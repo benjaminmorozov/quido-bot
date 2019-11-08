8f0bfa897afd8719dd0849668fd8438c120778d0
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-08 21:09:56 +0100

    npm snekfetch

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,22 +1,12 @@
 const snekfetch = require('snekfetch');
 
 exports.run = (client, message, args) => {
-    if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
-    return message.reply("sorry, you don't have enough permissions to use this!");
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
-    // To get the "message" itself we join the `args` back into a string with spaces:
-    if (!args.length) {
-        return;
-    }
-
-    const quidobot = "quido bot";
-    const quidobott = "bot quido";
-    if(message.content.toLowerCase().includes(quidobot) || message.content.toLowerCase().includes(quidobott)) {
-      message.delete();
-      return message.reply("well well... We do not say that here.")
-    };
-
-    message.delete();
-    let botmessage = args.join(" ");
-    message.channel.send(botmessage);
+  const { body } = await snekfetch
+      .get('https://www.reddit.com/r/hentai/hot.json')
+      .query({ limit: 800 });
+  const randomnumber = Math.floor(Math.random() * allowed.length)
+  const embed = new Discord.RichEmbed()
+  .setColor(0x00A2E8)
+  .setImage(allowed[randomnumber].data.url)
+  message.channel.send(embed)
 };
