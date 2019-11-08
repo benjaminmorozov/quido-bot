a7f4f9591b1f702ac8ee4aeab170643a6a8bceef
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-08 15:57:26 +0100

    test

--- commands/purge.js
+++ commands/purge.js
@@ -1,18 +1,18 @@
 exports.run = async (client, message, args) => {
     if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
     return message.reply("sorry, you don't have enough permissions to use this!");
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
+    // To get the "message" itself we join the `args` back into a string with spaces:
     if (!args.length) {
         return;
     }
     // get the delete count, as an actual number.
     const deleteCount = parseInt(args[0], 10);
-    
+
     // Ooooh nice, combined conditions. <3
     if(!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
+
     // So we get our messages, and delete them. Simple enough, right?
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
--- commands/say.js
+++ commands/say.js
@@ -1,11 +1,16 @@
 exports.run = (client, message, args) => {
     if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
     return message.reply("sorry, you don't have enough permissions to use this!");
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
+    // To get the "message" itself we join the `args` back into a string with spaces:
     if (!args.length) {
         return;
     }
+
+    if (args.includes(forbidenWords[i]) {
+      return message.reply("well well... We don't say that here.")
+    }
+
     message.delete();
     let botmessage = args.join(" ");
     message.channel.send(botmessage);
