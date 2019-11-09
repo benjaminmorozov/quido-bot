b78ea13829e29e151a74246f1a756efe3be5824c
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-09 16:11:50 +0100

    test

---
+++ commands/autodestruct.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    return message.reply("Destroying the server in 60 seconds. To stop the process, owner shall leave the server.")
+};
--- events/message.js
+++ events/message.js
@@ -4,7 +4,7 @@ module.exports = (client, message) => {
 
     // Ignore messages not starting with the prefix (in config.json)
     if (message.content.indexOf(client.config.prefix) !== 0) return;
-
+  
     // Our standard argument/command name definition.
     const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
@@ -14,12 +14,6 @@ module.exports = (client, message) => {
 
     // If that command doesn't exist, silently exit and do nothing
     if (!cmd) return;
-
-    const reload = "Prosím si jeden reload";
-    if(message.content.toLowerCase().includes(reload)) {
-      client.user.setPresence({ game: { name: "Quido's Club > All", type: 0 } });
-      return message.reply("Asi zablokuju možnost nastavit status.")
-    };
 
     // Run the command
     cmd.run(client, message, args);
