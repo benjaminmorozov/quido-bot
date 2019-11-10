00adc6c3aa6f7a4e9559b3557cdce5175c7d0c6e
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-10 11:56:18 +0100

    test

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -1,3 +1,5 @@
+const ms = require("ms");
+
 exports.run = async (client, message, args) => {
     if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
     return message.reply("sorry, you don't have enough permissions to use this!");
