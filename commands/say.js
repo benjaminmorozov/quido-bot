b988926afac8c2013e5357becd2285ed2d88360d
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-13 18:43:03 +0100

    test

--- commands/say.js
+++ commands/say.js
@@ -1,5 +1,6 @@
 exports.run = (client, message, args) => {
-    if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
+    const userID = '<@631205812005634058>';
+    if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) | !message.author === userID)
     return message.reply("sorry, you don't have enough permissions to use this!");
     // makes the bot say something and delete the message. As an example, it's open to anyone to use.
     // To get the "message" itself we join the `args` back into a string with spaces:
