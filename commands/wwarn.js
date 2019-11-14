d27c9b1d3bea6d0189d960597f04ff2fd1d508e0
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-14 16:19:06 +0100

    test

--- commands/wwarn.js
+++ commands/wwarn.js
@@ -1,4 +1,4 @@
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
     if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
     return message.reply("sorry, you don't have enough permissions to use this!");
 
