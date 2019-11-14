a74d7c7ec4dba8c8fdfd498e34624a844ee76776
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-14 16:27:05 +0100

    test

--- commands/wwarn.js
+++ commands/wwarn.js
@@ -1,31 +1,32 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('dmmi0dioj6mpk', 'sqknbmnacknhag', '0bcf997703be2c1aadb5bb882fa5d5ae2f46b23b710dfd6edc949bfcf9342908', {
-	host: 'ec2-54-228-252-67.eu-west-1.compute.amazonaws.com',
-	dialect: 'postgres',
-	logging: false,
-});
-const Tags = sequelize.define('tags', {
-	name: {
-		type: Sequelize.STRING,
-		unique: true,
-	},
-	description: Sequelize.TEXT,
-	username: Sequelize.STRING,
-	warns: {
-		type: Sequelize.INTEGER,
-		defaultValue: 0,
-		allowNull: false,
-	},
-});
-const splitArgs = args.split(' ');
-const tagName = splitArgs.shift();
-const tagDescription = splitArgs.join(' ');
 
 exports.run = async (client, message, args, sequelize) => {
     if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
     return message.reply("sorry, you don't have enough permissions to use this!");
+
+    const sequelize = new Sequelize('dmmi0dioj6mpk', 'sqknbmnacknhag', '0bcf997703be2c1aadb5bb882fa5d5ae2f46b23b710dfd6edc949bfcf9342908', {
+    	host: 'ec2-54-228-252-67.eu-west-1.compute.amazonaws.com',
+    	dialect: 'postgres',
+    	logging: false,
+    });
+    const Tags = sequelize.define('tags', {
+    	name: {
+    		type: Sequelize.STRING,
+    		unique: true,
+    	},
+    	description: Sequelize.TEXT,
+    	username: Sequelize.STRING,
+    	warns: {
+    		type: Sequelize.INTEGER,
+    		defaultValue: 0,
+    		allowNull: false,
+    	},
+    });
+    const splitArgs = args.split(' ');
+    const tagName = splitArgs.shift();
+    const tagDescription = splitArgs.join(' ');
 
     let member = message.mentions.members.first();
 
