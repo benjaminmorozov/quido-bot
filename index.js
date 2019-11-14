d65049c1191686ba5a689a6b465b475c98f5028b
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-14 16:04:28 +0100

    test

--- index.js
+++ index.js
@@ -9,13 +9,10 @@ const config = require("./config.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 
-const sequelize = new Sequelize('database', 'user', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
+const sequelize = new Sequelize('dmmi0dioj6mpk', 'sqknbmnacknhag', '0bcf997703be2c1aadb5bb882fa5d5ae2f46b23b710dfd6edc949bfcf9342908', {
+	host: 'ec2-54-228-252-67.eu-west-1.compute.amazonaws.com',
+	dialect: 'postgres',
 	logging: false,
-	// SQLite only
-	storage: 'database.sqlite',
-});
 
 /*
  * equivalent to: CREATE TABLE tags(
@@ -42,7 +39,7 @@ const Tags = sequelize.define('tags', {
 client.on('ready', () => {
   Tags.sync();
   console.log(chalk.redBright(`green`), `Successfully loaded and logged in as ${client.user.tag}.`);
-  client.user.setPresence({ game: { name: "q!help", type: 0 } });
+  client.user.setPresence({ game: { name: "Quido's Club > All", type: 0 } });
 });
 
 fs.readdir("./events/", (err, files) => {
