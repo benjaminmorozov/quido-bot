7445a050a8a9a3ba088e2f03af0de533da5231a3
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 23:03:45 +0100

    test

--- commands/dog.js
+++ commands/dog.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 const request = require('request');
 const fetch = require('node-fetch');
 exports.run = async (client, message, args) => {
-    const { message1 } = await fetch('https://aws.random.cat/meow').then(response => response.json());
-    const catEmbed = new Discord.RichEmbed()
+    const { url } = await fetch('https://random.dog/woof.json').then(response => response.json());
+    const dogEmbed = new Discord.RichEmbed()
         .setColor(0x333333)
         .setAuthor(`Here’s your Fresh and Instant Doggo ❤️:`)
-        .setImage(message1)
+        .setImage(url)
         .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
-    message.channel.send(catEmbed);
+    message.channel.send(dogEmbed);
 };
