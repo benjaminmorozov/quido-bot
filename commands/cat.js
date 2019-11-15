69ccac30ae716bfe90be1938837daf58627ed880
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 18:17:00 +0100

    test

--- commands/cat.js
+++ commands/cat.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
     const catEmbed = new Discord.RichEmbed()
       .setColor(0x333333)
       .setAuthor(`Here’s your Fresh and Instant Cat ❤️:`)
-      .setImage(url)
+      .setImage('${url}')
       .setFooter('If you find a bug, please report it to our staff. ❤️', 'https://cdn.discordapp.com/icons/610434388777369602/08a037cb16972aa3cd069a055d63ca43.webp');
     message.channel.send(catEmbed);
 };
