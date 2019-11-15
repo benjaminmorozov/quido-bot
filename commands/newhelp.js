502a03d115794af373b73142df44c2cb93920820
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 15:14:53 +0100

    test

--- commands/newhelp.js
+++ commands/newhelp.js
@@ -4,7 +4,7 @@ exports.run = (client, message) => {
   	.setColor('0xff5353')
   	.setTitle('🔰 List of all commands:')
   	.setThumbnail('http://giphygifs.s3.amazonaws.com/media/MF1kR4YmC2Z20/giphy.gif')
-  	.addField('Fun', 'q!', true)
+  	.addField('`Fun`', 'q!', true)
   	.addField('Misc', 'Some value here', true)
   	.addField('Mod', 'Some value here', true)
   	.setFooter('Thanks for being a part of our community. ❤️', 'https://images.discordapp.net/avatars/220644154177355777/bd6b28005c26d079486063a4976dfb44.png');
