8290c477e84ed8ff1c6e81ac6013b8c3bae9759e
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 15:50:11 +0100

    test

--- commands/setstatus.js
+++ commands/setstatus.js
@@ -1,10 +1,10 @@
 exports.run = (client, message, args) => {
     if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin"].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
-    
+
     const activity = args.join(" ");
     client.user.setActivity(activity);
-    message.channel.send(`My activity was set to "${activity}".`).then(sentMessage => {
+    message.channel.send(`My activity has been set to "${activity}".`).then(sentMessage => {
       sentMessage.delete(5000);
     });
 };
