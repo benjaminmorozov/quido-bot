012247825d9e42d9e64e3464038aab8172fe1521
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 18:30:08 +0100

    test

--- commands/cat.js
+++ commands/cat.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 const request = require('request');
-const random = require('node-dogandcat');
+const fetch = require('node-fetch');
 exports.run = (client, message, args) => {
-    let url = random.getRandomDog();
+    const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
     const catEmbed = new Discord.RichEmbed()
       .setColor(0x333333)
       .setAuthor(`Here’s your Fresh and Instant Cat ❤️:`)
-      .setImage('${url}')
+      .setImage(file)
       .setFooter('If you find a bug, please report it to our staff. ❤️', 'https://cdn.discordapp.com/icons/610434388777369602/08a037cb16972aa3cd069a055d63ca43.webp');
     message.channel.send(catEmbed);
 };
