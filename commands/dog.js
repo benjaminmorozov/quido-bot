6eb797d6328f3bd31ea46bab1c1bdbb092c8737e
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 23:07:33 +0100

    test

--- commands/dog.js
+++ commands/dog.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const request = require('request');
 const fetch = require('node-fetch');
 exports.run = async (client, message, args) => {
-    const { url } = await fetch('https://random.dog/woof.json').then(response => response.json());
+    const { url } = await fetch('https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1').then(response => response.json());
     const dogEmbed = new Discord.RichEmbed()
         .setColor(0x333333)
         .setAuthor(`Here’s your Fresh and Instant Doggo ❤️:`)
