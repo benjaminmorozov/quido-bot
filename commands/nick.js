5e5a4f7c946411b487897f6ef6aebb8b89893c19
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 17:44:36 +0100

    test

--- commands/nick.js
+++ commands/nick.js
@@ -1,17 +1,4 @@
 exports.run = (client, message, args) => {
-    const taggedUser = message.mentions.users.first() || message.author;
-    let nick = args.join(" ").slice(22);
-    if(!message.author.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) || taggedUser === message.mentions.users.first())
-    return message.reply("sorry, you don't have enough permissions to use this!");
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
-    // To get the "message" itself we join the `args` back into a string with spaces:
-    if (!args.length) {
-        return;
-    }
-
-    if(message.author.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) || taggedUser === message.mentions.users.first())
-    message.guild.members.get(taggedUser.id).setNickname(nick);
-
-    if(!message.author.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) || taggedUser === message.author)
-    message.guild.members.get(message.author.id).setNickname(nick);
+  const taggedUser = message.author;
+  
 };
--- commands/say.js
+++ commands/say.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
-    return message.reply("sorry, you don't have enough permissions to use this!");
+    return message.reply("you don't have enough permissions to use this!");
     // makes the bot say something and delete the message. As an example, it's open to anyone to use.
     // To get the "message" itself we join the `args` back into a string with spaces:
     if (!args.length) {
