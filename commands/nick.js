f9a1a61e2b764350d1c580c534ff42ed856d79d5
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 17:48:13 +0100

    test

--- commands/nick.js
+++ commands/nick.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args) => {
   const user = message.author;
   let reason = args.join(" ").slice(22);
-  message.user.setNickname(reason);
+  message.guild.members.get(user.id).setNickname(reason);
 };
