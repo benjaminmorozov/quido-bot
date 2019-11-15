aceb9068f6a965333c8349a12ff494d33148a20f
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 18:45:56 +0100

    test

--- commands/dog.js
+++ commands/dog.js
@@ -1,22 +1,12 @@
 const Discord = require("discord.js");
 const request = require('request');
-const rp = require('random-puppy');
-exports.run = (client, message, args) => {
-  var subreddits = [
-    'rarepuppers'
-  ]
-  var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
-  try {
-    rp(sub).then(url=> {
-      const dogEmbed = new Discord.RichEmbed()
+const fetch = require('node-fetch');
+exports.run = async (client, message, args) => {
+    const { message } = await fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json());
+    const dogEmbed = new Discord.RichEmbed()
       .setColor(0x333333)
-      .setAuthor(`Here’s your Fresh and Instant Doggo ❤️:`)
-      .setImage(url)
-      .setFooter('If you find a bug, please report it to our staff. ❤️', 'https://cdn.discordapp.com/icons/610434388777369602/08a037cb16972aa3cd069a055d63ca43.webp');
+      .setAuthor(`Here’s your Fresh and Instant Cat ❤️:`)
+      .setImage(message)
+      .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
     message.channel.send(dogEmbed);
-              })
-          ;
-  } catch(e) {
-    console.log(e);
-  };
 };
