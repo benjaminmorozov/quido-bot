b703d36cd9f28063b582817bf1f1d03c4e2ed273
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 15:37:38 +0100

    test

--- commands/newhelp.js
+++ commands/newhelp.js
@@ -9,7 +9,7 @@ exports.run = (client, message) => {
   	.setThumbnail('http://giphygifs.s3.amazonaws.com/media/MF1kR4YmC2Z20/giphy.gif')
   	.addField('# **Fun**', '`q!8ball` - Ask the mystic 8-ball a question.\n`q!cat` - Display a random picture of a cute cat.\n`q!dog` - Display a random picture of a cute doggo.\n`q!nsfw` - Display a random NSFW picture.', false)
   	.addField('# **Misc**', '`q!avatar` - Display the avatar of any member of this server.\n`q!help` - Display a list of all the commands.\n`q!server` - Display the server info.\n`q!userinfo` - Display some info about a member of this server.', false)
-  	.setFooter('Thanks for being a part of our community. ❤️', 'https://images.discordapp.net/avatars/220644154177355777/bd6b28005c26d079486063a4976dfb44.png');
+  	.setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
 
   message.channel.send(baseEmbed);
 };
--- commands/server.js
+++ commands/server.js
@@ -11,27 +11,27 @@ exports.run = (client, message) => {
         description: "A Czech/English gaming Discord server for everyone.",
         fields: [{
             name: "Server Owner:",
-            value: "<@529057345599307776>",
+            value: "`<@529057345599307776>`",
             inline: "true"
             },
             {
             name: "Bot Prefix:",
-            value: `**${client.config.prefix}**`,
+            value: ``**${client.config.prefix}**``,
             inline: "true"
             },
             {
             name: "User Count:",
-            value: `${message.guild.members.filter(member => !member.user.bot).size}`,
+            value: ``${message.guild.members.filter(member => !member.user.bot).size}``,
             inline: "true"
             },
             {
             name: "Staff List:",
-            value: "<#638804758240559154>",
+            value: "`<#638804758240559154>`",
             inline: "true"
             },
             {
             name: "Server Creation Date:",
-            value: `${message.channel.guild.createdAt.toUTCString().substr(0, 16)}`,
+            value: ``${message.channel.guild.createdAt.toUTCString().substr(0, 16)}``,
             inline: "true"
             },
         ],
