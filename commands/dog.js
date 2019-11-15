4fd2cf85ad95414b7365216734911355fa3ff921
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 18:51:24 +0100

    test

--- commands/dog.js
+++ commands/dog.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
     const { url } = await fetch('https://random.dog/woof.json').then(response => response.json());
     const dogEmbed = new Discord.RichEmbed()
       .setColor(0x333333)
-      .setAuthor(`Here’s your Fresh and Instant Cat ❤️:`)
+      .setAuthor(`Here’s your Fresh and Instant Doggo ❤️:`)
       .setImage(url)
       .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
     message.channel.send(dogEmbed);
