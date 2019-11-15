dd48db5dcea06d2272a11cde3207cd3b8dfc78c5
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 18:47:20 +0100

    test

--- commands/dog.js
+++ commands/dog.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const request = require('request');
 const fetch = require('node-fetch');
-exports.run = async (client, message, args) => {
+exports.run = async (client, args) => {
     const { message } = await fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json());
     const dogEmbed = new Discord.RichEmbed()
       .setColor(0x333333)
