72ffb4120293b17bb7d73fdd52d72dbbc3c4e9cc
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 22:53:10 +0100

    test

--- commands/dog.js
+++ commands/dog.js
@@ -1,12 +1,11 @@
 const Discord = require("discord.js");
 const animals = require('random-animals-api');
-exports.run = (client, message, args) => {
-    animals.dog()
-        .then(url =>
-    const dogEmbed = new Discord.RichEmbed()
-      .setColor(0x333333)
-      .setAuthor(`Here’s your Fresh and Instant Doggo ❤️:`)
-      .setImage(url)
-      .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`););
+exports.run = async (client, message, args) => {
+    animals.cat()
+        .then(url =>     const dogEmbed = new Discord.RichEmbed()
+        .setColor(0x333333)
+        .setAuthor(`Here’s your Fresh and Instant Doggo ❤️:`)
+        .setImage(url)
+        .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);)
     message.channel.send(dogEmbed);
 };
