643aaacf0d69d5b1a98cface76226382a97a7132
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-15 17:30:24 +0100

    test

--- commands/cat.js
+++ commands/cat.js
@@ -1,12 +1,18 @@
 const Discord = require("discord.js");
 const request = require('request');
-var randomCat = require('random-cat');
 exports.run = (client, message, args) => {
-    var url = randomCat.get();
+  request.get('http://thecatapi.com/api/images/get?format=src&type=png', {
+
+  }, function(error, response, body) {
+    if(!error && response.statusCode == 200) {
     const catEmbed = new Discord.RichEmbed()
       .setColor(0x333333)
       .setAuthor(`Here’s your Fresh and Instant Cat ❤️:`)
-      .setImage(url)
+      .setImage(response.request.uri.href)
       .setFooter('If you find a bug, please report it to our staff. ❤️', 'https://cdn.discordapp.com/icons/610434388777369602/08a037cb16972aa3cd069a055d63ca43.webp');
     message.channel.send(catEmbed);
+    } else {
+      console.log(error);
+    }
+  })
 };
---
+++ commands/nick.js
@@ -0,0 +1,17 @@
+exports.run = (client, message, args) => {
+    const taggedUser = message.mentions.users.first() || message.author;
+    let nick = args.join(" ").slice(22);
+    if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) || taggedUser = message.mentions.users.first())
+    return message.reply("sorry, you don't have enough permissions to use this!");
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
+    // To get the "message" itself we join the `args` back into a string with spaces:
+    if (!args.length) {
+        return;
+    }
+
+    if(message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) || taggedUser = message.mentions.users.first())
+    taggedUser.setNickname(nick);
+
+    if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) || taggedUser = message.author)
+    message.author.setNickname(nick);
+};
