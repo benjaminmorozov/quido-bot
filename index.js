5e363eda883288a7ec639ecc015e7afd0972ba8d
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-16 22:55:07 +0100

    test

--- index.js
+++ index.js
@@ -39,10 +39,17 @@ const config = require("./config.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 
+AntiSpam.on("warnEmit", (member) => console.log(Attempt to warn ${member.user.tag}.));
+AntiSpam.on("warnAdd", (member) => console.log(${member.user.tag} has been warned.));
+
 client.on('ready', () => {
   tags.sync();
   console.log(`Successfully loaded and logged in as ${client.user.tag}.`);
   client.user.setPresence({ game: { name: "Quido's Club > All", type: 0 } });
+});
+
+client.on("message", (msg) => {
+AntiSpam.message(msg);
 });
 
 fs.readdir("./events/", (err, files) => {
