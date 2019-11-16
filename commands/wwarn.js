25207fa923f09f36b9d2ead90b9b3a17cfc213f3
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-16 18:31:51 +0100

    test

--- commands/wwarn.js
+++ commands/wwarn.js
@@ -2,31 +2,11 @@ const Discord = require("discord.js");
 const Enmap = require("enmap");
 const Sequelize = require('sequelize');
 
-exports.run = async (client, message, args, Sequelize, db) => {
+exports.run = async (client, message, args) => {
     if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
     return message.reply("sorry, you don't have enough permissions to use this!");
 
     let member = message.mentions.members.first();
-
-    const tags = db.define('tags', {
-      name: {
-        type: Sequelize.STRING,
-        unique: true,
-      },
-      description: Sequelize.TEXT,
-      username: Sequelize.STRING,
-      warns: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-      },
-    });
-
-    const tag = await tags.create({
-      name: member.username,
-      description: 'warns',
-      username: member.id,
-    });
 
     return message.reply(`db check successful.`);
 };
