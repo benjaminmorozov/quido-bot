1cea975b047255527c088951111670804d5bc438
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-16 22:25:04 +0100

    test

--- index.js
+++ index.js
@@ -3,15 +3,24 @@ const Enmap = require("enmap");
 var Sequelize = require('sequelize');
 const fs = require("fs");
 const { RichEmbed } = require('discord.js');
-
 const client = new Discord.Client();
-
+const AntiSpam = new DiscordAntiSpam({
+  warnThreshold: 3, // Amount of messages sent in a row that will cause a warning.
+  maxInterval: 2000, // Amount of time (in ms) in which messages are cosidered spam.
+  warnMessage: "{@user}, please stop spamming.", // Message will be sent in chat upon warning.
+  maxDuplicatesWarning: 4, // Amount of same messages sent that will be considered as duplicates that will cause a warning.
+  exemptPermissions: ["MANAGE_MESSAGES", "ADMINISTRATOR", "MANAGE_GUILD", "BAN_MEMBERS"], // Bypass users with at least one of these permissions
+  ignoreBots: true, // Ignore bot messages
+  verbose: false, // Extended Logs from module
+  ignoredUsers: [529057345599307776,524939155117965312,315552443515535361,203149905370415104,207942673900765184,339714610108039168,207044528027205634,547747425712996352,301376346335084555,282413588562313218,278210468362059777,328875130010992640,312242014387175424,429671461213437954,490962890124165156,368696360444362753,382558580093616128,,325739426191900677336867477718040600,350945463546937344,493159676620570636], // Array of string user IDs that are ignored
+  ignoredGuilds: [], // Array of string Guild IDs that are ignored
+  ignoredChannels: [] // Array of string channels IDs that are ignored
+});
 const sequelize = new Sequelize('dmmi0dioj6mpk', 'sqknbmnacknhag', '0bcf997703be2c1aadb5bb882fa5d5ae2f46b23b710dfd6edc949bfcf9342908', {
   host: 'ec2-54-228-252-67.eu-west-1.compute.amazonaws.com',
   dialect: 'postgres',
   logging: false,
 });
-
 const tags = sequelize.define('tags', {
   name: {
     type: Sequelize.STRING,
@@ -25,7 +34,6 @@ const tags = sequelize.define('tags', {
     allowNull: false,
   },
 });
-
 const config = require("./config.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
