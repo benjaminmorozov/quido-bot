88cce1310bbffa47ac593ee7ffcba6472693d3c4
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-16 18:11:45 +0100

    test

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ const sequelize = new Sequelize('dmmi0dioj6mpk', 'sqknbmnacknhag', '0bcf997703be
   logging: false,
 });
 
+const tags = sequelize.define('tags', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+  },
+  description: Sequelize.TEXT,
+  username: Sequelize.STRING,
+  warns: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    allowNull: false,
+  },
+});
+
 const config = require("./config.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
