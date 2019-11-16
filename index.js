b23bce2ee829803c78cf983060555468bc9b514e
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-16 22:58:42 +0100

    test

--- index.js
+++ index.js
@@ -39,9 +39,12 @@ const config = require("./config.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 
-AntiSpam.on("warnEmit", (member) => console.log(Attempt to warn ${member.user.tag}.));
-AntiSpam.on("warnAdd", (member) => console.log(${member.user.tag} has been warned.));
-
+AntiSpam.on("warnEmit", (member) =>  {
+  console.log(`Attempt to warn ${member.user.tag}.`)
+});
+AntiSpam.on("warnAdd", (member) => {
+  console.log(`${member.user.tag} has been warned.`)
+});
 client.on('ready', () => {
   tags.sync();
   console.log(`Successfully loaded and logged in as ${client.user.tag}.`);
