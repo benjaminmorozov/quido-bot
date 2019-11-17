555287bf0c35d39ff1cec8314f09f0010a9f099d
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-17 22:27:33 +0100

    test

--- commands/kick.js
+++ commands/kick.js
@@ -1,5 +1,5 @@
 exports.run = async (client, message, args) => {
-    if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator","Head Admin","Admin"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator","Head Admin","Admin","Head Moderator","Moderator"].includes(r.name)) )
     return message.reply("you don't have enough permissions to execute this command!");
 
     // Let's first check if we have a member and if we can kick them!
