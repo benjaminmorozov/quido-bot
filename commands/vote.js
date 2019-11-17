3b12166bf65772b7b10b69dd3f9ad7b31fb0912c
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-17 22:10:26 +0100

    test

--- commands/vote.js
+++ commands/vote.js
@@ -1,4 +1,4 @@
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
     if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
     return message.reply("you don't have enough permissions to execute this command!");
 
@@ -6,6 +6,7 @@ exports.run = (client, message, args) => {
     if(!reason)
       return message.reply("you must have something to vote for!");
 
+    message.delete();
     const pollTopic = await message.channel.send(reason);
     await pollTopic.react(`✅`);
     await pollTopic.react(`⛔`);
