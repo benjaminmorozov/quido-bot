f5bd62709921d8317eb88931a43820bd340f18a7
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-17 02:28:21 +0100

    test

--- commands/bcreate.js
+++ commands/bcreate.js
@@ -1,16 +1,9 @@
-const backup = require("discord-backup");
-exports.run = async (client, message, args, backup) => {
+exports.run = (client, message, args, backup) => {
   /**
  * @param {object} [Guild] - The discord server you want to backup
  */
- backup.create(message.guild).then((backupID) => {
-   const backupEmbed = new Discord.RichEmbed()
-   	.setColor('0xff5353')
-   	.setTitle('⚠️ Backup:')
-   	.setThumbnail(`${client.user.avatarURL}`)
-    .setDescription(`Created a new backup for ${guild.name}.`)
-   	.addField('Backup ID:', backupID, false)
-   	.setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
-   message.channel.send(backupEmbed);
+ const backup = require("discord-backup");
+ backup.create(Guild).then((backupID) => {
+     console.log(backupID); // NSJH2
  });
 };
