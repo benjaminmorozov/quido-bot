f9dc98901cd5cd2eedf08dab0c0bee93ff352972
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-17 02:24:02 +0100

    test

--- commands/bcreate.js
+++ commands/bcreate.js
@@ -3,7 +3,6 @@ exports.run = async (client, message, args, backup) => {
   /**
  * @param {object} [Guild] - The discord server you want to backup
  */
- backup.setStorageFolder(__dirname+"/backups/");
  await backup.create(Guild).then((backupID) => {
    const backupEmbed = new Discord.RichEmbed()
    	.setColor('0xff5353')
@@ -12,7 +11,7 @@ exports.run = async (client, message, args, backup) => {
     .setDescription(`Created a new backup for ${guild.name}.`)
    	.addField('Backup ID:', backupID, false)
    	.setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
-
+   backup.setStorageFolder(__dirname+"/backups/");
    message.channel.send(backupEmbed);
  });
 };
