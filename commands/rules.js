e65fb517f52fa0b4eaf7ddbb5e0dd49055289542
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-19 18:49:50 +0100

    commit

--- commands/rules.js
+++ commands/rules.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 exports.run = (client, message) => {
   const baseEmbed = new Discord.RichEmbed()
-  	.setColor('0xff5353')
-  	.setTitle('🔰 List of all commands:')
-  	.setThumbnail('http://giphygifs.s3.amazonaws.com/media/MF1kR4YmC2Z20/giphy.gif')
+  	.setColor('0xff5353'
   	.addField('**Server Rules:**', '`q!8ball` - Ask the mystic 8-ball a question.\n`q!cat` - Display a random picture of a cute cat.\n`q!dog` - Display a random picture of a cute doggo.\n`q!nsfw` - Display a random NSFW picture.', false)
   	.setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
 
