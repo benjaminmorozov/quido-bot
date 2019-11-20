e25fa4d59c87537d745b9f5617bdf89179e7cbd5
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-20 14:59:43 +0100

    test

--- commands/gcreate.js
+++ commands/gcreate.js
@@ -2,7 +2,8 @@ exports.run = (client, message, args) => {
     if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator","Head Moderator","Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
     return message.reply("sorry, you don't have enough permissions to use this!");
 
-    message.channel.send(`:beginner:| Nice, so we're going to make a giveaway! In what channel?`).then(msg => {
+    var filter = m => m.author.id === message.author.id;
+    message.channel.send(`🎉 Nice, so we're going to make a giveaway! In what channel?`).then(msg => {
       message.channel.awaitMessages(filter, {
         max: 1,
         time: 20000,
