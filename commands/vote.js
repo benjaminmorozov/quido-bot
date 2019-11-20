9138a633604e97acb247c989ffbddbffc45f4b06
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-20 21:16:40 +0100

    test

--- commands/vote.js
+++ commands/vote.js
@@ -2,8 +2,6 @@ exports.run = async (client, message, args) => {
     let reason = args.join(" ");
     if(!reason) {
       return message.reply("you must have something to vote for!");
-    } else {
-      if (!message.content.includes("?")) return message.reply("a vote must have an exclamation mark!");
     };
 
     message.delete();
