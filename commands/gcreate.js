27c0c14a7da7f19e7dd806dd6a533a1face903e6
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-20 15:14:59 +0100

    test

--- commands/gcreate.js
+++ commands/gcreate.js
@@ -11,7 +11,7 @@ exports.run = (client, message, args) => {
       }).then(collected => {
         var step = collected.first().content;
         let channel = step.replace('#', '');
-        let room = message.guild.channels.find('id' , channel);
+        let room = message.guild.channels.find('name' , channel);
         if(!room) return message.channel.send(':disappointed_relieved: I could not find that channel');
       });
     });
