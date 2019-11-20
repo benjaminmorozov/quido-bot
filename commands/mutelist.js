981f39dc7698e5f065aa751bf2fddae1d8b8cafb
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-20 14:27:25 +0100

    test

--- commands/mutelist.js
+++ commands/mutelist.js
@@ -3,14 +3,25 @@ exports.run = async (client, message) => {
   if(!message.member.roles.some(r=>["610704273822711820","622715668659437568","631922921475932170","616501517058310184","645728270519631889","610704593558437899","614694328119459840"].includes(r.id)) )
     return message.reply("you don't have enough permissions to execute this command!");
 
+  const muted = message.guild.roles.get('630146706930925569').members.map(m=>m.user.tag).join('\n');
+  if(!muted)
     message.channel.send({embed: {
-        color: 0xff5353,
-        title: 'All muted members on this server:',
-        description: `**> ${message.guild.roles.get('630146706930925569').members.map(m=>m.user.tag).join('\n')}**`,
-        footer: {
-          icon_url: `${client.user.avatarURL}`,
-          text: "Thanks for being a part of our community ❤️"
-        }
+      color: 0xff5353,
+      title: 'There are no muted members on this server.',
+      footer: {
+        icon_url: `${client.user.avatarURL}`,
+        text: "Thanks for being a part of our community ❤️"
       }
-    });
+    }
+  });
+  message.channel.send({embed: {
+      color: 0xff5353,
+      title: 'All muted members on this server:',
+      description: `**> ${muted}**`,
+      footer: {
+        icon_url: `${client.user.avatarURL}`,
+        text: "Thanks for being a part of our community ❤️"
+      }
+    }
+  });
 };
