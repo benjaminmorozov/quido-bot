9d352efd695258953b3865a8b3cfd14b607f2cd4
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-21 19:57:46 +0100

    test

--- commands/epic.js
+++ commands/epic.js
@@ -1,14 +1,13 @@
-const Discord = require("discord.js");
-exports.run = (client, message) => {
-  const baseEmbed = new Discord.RichEmbed()
-  	.setColor('0xff5353')
-    .setDescription('Bad North is now free on Epic Games Store until the 29th of November. After claiming, the game\'s yours to keep forever.')
-  	.setThumbnail('https://cdn.discordapp.com/attachments/638791812038918174/647133768553070628/img.png')
-  	.addField('**What?**', 'Bad North', false)
-  	.addField('**Where?**', 'Epic Games Store', false)
-  	.addField('**Until when?**', 'until the 29th of November', false)
-  	.addField('**:O**', '[Bad North on Epic Games Store](https://www.epicgames.com/store/en-US/product/bad-north/)', false)
-  	.setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
-
-  message.channel.send(baseEmbed);
-};
+//const Discord = require("discord.js");
+//exports.run = (client, message) => {
+//  const baseEmbed = new Discord.RichEmbed()
+//  	.setColor('0xff5353')
+//    .setDescription('Bad North is now free on Epic Games Store until the 29th of November. After claiming, the game\'s yours to keep forever.')
+//  	.setThumbnail('https://cdn.discordapp.com/attachments/638791812038918174/647133768553070628/img.png')
+//  	.addField('**What?**', 'Bad North', false)
+//  	.addField('**Where?**', 'Epic Games Store', false)
+//  	.addField('**Until when?**', 'until the 29th of November', false)
+//  	.addField('**:O**', '[Bad North on Epic Games Store](https://www.epicgames.com/store/en-US/product/bad-north/)', false)
+//  	.setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
+//  message.channel.send(baseEmbed);
+//};
--- index.js
+++ index.js
@@ -14,6 +14,34 @@ client.on('ready', () => {
   client.user.setPresence({ game: { name: "Quido's Club > All", type: 0 } });
 });
 
+//message deleted
+bot.on('messageDelete', function(message) {
+
+    if(message.channel.type == 'text') {
+
+        //post in the guild's log channel            #logs
+        var log = message.guild.channels.find('id', '617351547130478621');
+        if (log != null)
+            log.sendMessage('**[Message Deleted]** ' + message.author + ': ' + message.cleanContent);
+
+    }
+
+});
+
+//message update
+client.on('messageUpdate', function(oldMessage, newMessage) {
+
+    if (newMessage.channel.type == 'text' && newMessage.cleanContent != oldMessage.cleanContent) {
+
+        //post in the guild's log channel               #logs
+        var log = newMessage.guild.channels.find('id', '617351547130478621');
+        if (log != null)
+            log.sendMessage('**[Message Updated]** *' + newMessage.author + '*:\n*Old Message*: ' + oldMessage.cleanContent +
+                '\n*New Message*: ' + newMessage.cleanContent);
+    }
+
+});
+
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
