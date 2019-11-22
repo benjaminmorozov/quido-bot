bdb9f1c725bd23a3e99a18d886dc48f67de74d60
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-22 17:14:41 +0100

    test

--- commands/invite.js
+++ commands/invite.js
@@ -3,5 +3,5 @@ exports.run = async (client, message, args) => {
       maxAge: 0, //maximum time for the invite, in milliseconds
       maxUses: 1 //maximum times it can be used
     }, `Requested with command by ${message.author.tag}`).catch(console.log);
-  message.reply(invite ? `Created a new one-use invite: ${invite}` : "There has been an error during the creation of the invite.");
+  message.reply(invite ? `created a new one-use invite for you: ${invite}` : "There has been an error during the creation of the invite.");
 };
