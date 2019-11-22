b9e16f0dc8934af1066669c17abb1022746c4a38
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-22 16:22:41 +0100

    test

--- commands/invite.js
+++ commands/invite.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args) => {
   let invite = await message.channel.createInvite({
-      maxAge: 10 * 60 * 1000, //maximum time for the invite, in milliseconds
+      maxAge: 0, //maximum time for the invite, in milliseconds
       maxUses: 1 //maximum times it can be used
     }, `Requested with command by ${message.author.tag}`).catch(console.log);
   message.reply(invite ? `Created a new one-use invite: ${invite}` : "There has been an error during the creation of the invite.");
