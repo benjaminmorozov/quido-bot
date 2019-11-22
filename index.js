f5fc6b2423dbd248d66545db2defeb0423659a34
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-22 20:33:45 +0100

    test

--- index.js
+++ index.js
@@ -24,9 +24,14 @@ client.on('messageDelete', function(message) {
       var log = message.guild.channels.find('id', '617351547130478621');
       if (log != null) {
         message.attachments.forEach(a => {
-          request.get(a.url)
-              .on('error', console.error)
-              .pipe(fs.createWriteStream(`${a.filename}`));
+          if(msg.attachments.first().filename === `png`){//Download only png (customize this)
+            function download(url){
+              request.get(url)
+                .on('error', console.error)
+                .pipe(fs.createWriteStream(`${a.filename}`));
+              }
+              download(msg.attachments.first().url);//Function I will show later
+          };
           const attachment = new Discord.Attachment(`./${a.filename}`);
       		log.send(`${message.author},`, attachment);
         });
