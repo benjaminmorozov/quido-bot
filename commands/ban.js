64490aaf18f7e314e82062533ceb5cfe9eaf7822
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-23 14:15:41 +0100

    test

--- commands/ban.js
+++ commands/ban.js
@@ -46,7 +46,7 @@ exports.run = async (client, message, args) => {
       .catch(error => message.channel.send(`Sorry ${message.author}, I couldn't ban ${member.user} because of : ${error}`));
     message.channel.send(`${member.user} has been banned by ${message.author} because: ${reason}`);
   } else {
-    if(member.roles.has('613347276647039016') {
+    if(member.roles.has('613347276647039016')) {
       await member.ban(reason)
         .catch(error => message.channel.send(`Sorry ${message.author}, I couldn't ban ${member.user} because of : ${error}`));
       message.channel.send(`${member.user} has been banned by ${message.author} because: ${reason}`);
