08801237a37ec612b828e67024f2a46be5ce0f44
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-23 19:56:18 +0100

    test

--- commands/hentai.js
+++ commands/hentai.js
@@ -1,22 +1,14 @@
-const randomPuppy = require('random-puppy');
-const request = require('snekfetch');
-const fs = require("fs")
+const discord = require('discord.js');
+const superagent = require('superagent')
 
-exports.run = (client, message, args) => {
-    if (!message.channel.nsfw) return message.channel.send(":underage: NSFW Command. Please switch to NSFW channel in order to use this command.")
-
-    var subreddits = [
-        'HENTAI_GIF',
-        'hentai_irl'
-    ]
-    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
-
-    randomPuppy(sub)
-        .then(url => {
-            request.get(url).then(r => {
-                fs.writeFile(`hentai.jpg`, r.body)
-                message.channel.sendFile(r.body)
-                fs.unlink(`./hentai.jpg`)
-            })
-        })
-}
+exports.run = (client, msg, args) => {
+  if (msg.channel.nsfw === true) {
+    superagent.get('https://nekobot.xyz/api/image')
+    .query({ type: 'hentai_anal'})
+    .end((err, response) => {
+      msg.channel.send({ file: response.body.message });
+    });
+  } else {
+    msg.channel.send("This isn't NSFW channel!")
+  }
+};
