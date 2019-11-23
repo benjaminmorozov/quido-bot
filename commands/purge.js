123ceb4fe9ff5483f43b17e71db6b2f11ff1ab88
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-23 21:55:40 +0100

    test

--- commands/purge.js
+++ commands/purge.js
@@ -10,7 +10,7 @@ exports.run = async (client, message, args) => {
     const deleteCount = parseInt(args[0], 10);
 
     // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+    if(!deleteCount || deleteCount < 2)
       return message.reply("provide a number between 2 and 100 for the number of messages to delete.");
 
     // So we get our messages, and delete them. Simple enough, right?
