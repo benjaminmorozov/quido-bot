9b1d1e511464de11732b95a7da4fb3bf50013ff0
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-27 18:33:33 +0100

    test

--- commands/bugreport.js
+++ commands/bugreport.js
@@ -1,9 +1,10 @@
 exports.run = (client, message, args) => {
-  if (!args.length) {
+    if (!args.length) {
       return;
-  }
-  let reportmessage = args.join(" ");
-  client.fetchUser("207044528027205634",false).then(user => {
-          user.send(reportmessage);
-  });
+    }
+    let reportmessage = args.join(" ");
+    client.fetchUser("207044528027205634",false).then(user => {
+      user.send(reportmessage);
+    });
+    message.reply("bug report was sent to the staff ❤️. Thanks for being a part of our community.");
 };
