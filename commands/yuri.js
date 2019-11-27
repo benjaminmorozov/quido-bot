dcc91616461213efd9080d5477912f98dcf5d825
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-27 19:43:20 +0100

    test

--- commands/yuri.js
+++ commands/yuri.js
@@ -2,7 +2,7 @@ const superagent = require("snekfetch");
 const Discord = require('discord.js')
 
 exports.run = async (client, message, args, level) => {
-    if (!message.channel.nsfw) return message.channel.send('You can use this command in an NSFW Channel!')
+    if (!message.channel.nsfw) return message.channel.send(':underage: NSFW Command. Please switch to NSFW channel in order to use this command.')
     superagent.get('https://nekos.life/api/v2/img/yuri')
         .end((err, response) => {
       const lewdembed = new Discord.RichEmbed()
