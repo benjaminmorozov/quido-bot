bd1ab3d831ca2aa3c04357d227cc2d3fcea1b10b
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-28 20:42:27 +0100

    test

--- commands/remind.js
+++ commands/remind.js
@@ -1,17 +1,59 @@
-const { stripIndents } = require('common-tags');
-const moment = require('moment');
-const sherlock = require('sherlockjs');
 exports.run = async (client, message, args, { remind }) => {
-  let reason = args.join(" ").slice(22);
-  const time = remind.startDate.getTime() - Date.now();
-  const preRemind = await msg.say(stripIndents`
-    I will remind you '${Util.cleanContent(message, remind.eventTitle)}' ${moment().add(time, 'ms').fromNow()}.
-  `);
-  const remindMessage = await new Promise(resolve => {
-    setTimeout(() => resolve(message.say(stripIndents`
-      ${message.author} you wanted me to remind you of: '${Util.cleanContent(message, remind.eventTitle)}'
-    `)), time);
-  });
+  var message = msg;
+  try {
 
-  return [preRemind, remindMessage];
+    // Variables
+    var returntime;
+    var timemeasure;
+    var userid;
+    msg = msg.content.split(' ');
+    console.log('Message recieved from ' + message.author.id + ' at ' + Date.now().toString());
+
+    // Sets the userid for the recipiant
+    userid = client.users.get(msg[1].replace('<@!', '').slice(0, -1))
+
+    // Sets the return time
+    timemeasure = msg[2].substring((msg[2].length - 1), (msg[2].length))
+    returntime = msg[2].substring(0, (msg[2].length - 1))
+
+    // Based off the delimiter, sets the time
+    switch (timemeasure) {
+      case 's':
+        returntime = returntime * 1000;
+        break;
+
+      case 'm':
+        returntime = returntime * 1000 * 60;
+        break;
+
+      case 'h':
+        returntime = returntime * 1000 * 60 * 60;
+        break;
+
+      case 'd':
+        returntime = returntime * 1000 * 60 * 60 * 24;
+        break;
+
+      default:
+        returntime = returntime * 1000;
+        break;
+    }
+
+    // Returns the Message
+    client.setTimeout(function () {
+      // Removes the first 2 array items
+      msg.shift();
+      msg.shift();
+      msg.shift();
+
+      // Creates the message
+      var content = msg.join();
+      content = content.replaceAll(',', ' ');
+      message.channel.send(userid + content);
+      console.log('Message sent to ' + userid + ' at ' + Date.now().toString());
+    }, returntime)
+  } catch (e) {
+    message.reply("An error has occured, please make sure the command has a time delimiter and message");
+    console.error(e.toString());
+  }
 };
