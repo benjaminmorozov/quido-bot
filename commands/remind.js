c867b0eee27e199474431eb476f1b31f6a5dca43
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-28 20:37:10 +0100

    test

---
+++ commands/remind.js
@@ -0,0 +1,16 @@
+const { stripIndents } = require('common-tags');
+const moment = require('moment');
+const sherlock = require('sherlockjs');
+exports.run = async (client, message, args) => {
+  const time = remind.startDate.getTime() - Date.now();
+  const preRemind = await msg.say(stripIndents`
+    I will remind you '${Util.cleanContent(message, remind.eventTitle)}' ${moment().add(time, 'ms').fromNow()}.
+  `);
+  const remindMessage = await new Promise(resolve => {
+    setTimeout(() => resolve(message.say(stripIndents`
+      ${message.author} you wanted me to remind you of: '${Util.cleanContent(message, remind.eventTitle)}'
+    `)), time);
+  });
+
+  return [preRemind, remindMessage];
+};
