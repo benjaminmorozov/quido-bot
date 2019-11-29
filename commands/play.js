905d9753e55952cfb47733955d3bc75c9df7c53d
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-29 16:58:51 +0100

    test

--- commands/play.js
+++ commands/play.js
@@ -1,3 +1,24 @@
 const ytdl = require('ytdl-core');
 exports.run = async (client, message, args) => {
+  if(!message.member.voiceChannel)
+    return message.channel.send('Please, connect to a voice channel.');
+
+  if(message.guild.me.voiceChannel)
+    return message.channel.send('Sorry, the bot is already connected to a voice channel.');
+
+  if(!args[0])
+    return message.channel.send('Sorry, please input a url following the command.');
+
+  let validate = await ytdl.validateURL(args[0]);
+
+  if(!validate)
+    return message.channel.send('Sorry, please input a **valid** url following the command.');
+
+  let info = await ytdl.getInfo(args[0]);
+
+  let connection = await message.member.voiceChannel.join();
+
+  let dispatcher = await connection.play(ytdl(args[0], {filter: 'audioonky'}));
+
+  message.channel.send(`Now playing: ${info.title}`);
 };
