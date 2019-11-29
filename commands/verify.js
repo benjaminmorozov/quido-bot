c40f746adab57a514a3eb779b907d16ea6b3322f
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-30 00:30:28 +0100

    test

--- commands/verify.js
+++ commands/verify.js
@@ -2,7 +2,6 @@ const Discord = require("discord.js");
 exports.run = async (client, message, args) => {
   let reason = args.join(" ");
   const member = message.author;
-  member.send(reason);
   if(message.guild === null) {
     if(reason === code) {
       const verifyEmbed = new Discord.RichEmbed()
@@ -12,7 +11,7 @@ exports.run = async (client, message, args) => {
         .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
       member.send(verifyEmbed);
       let verifiedRole = client.guilds.get("610434388777369602").roles.find(role => role.id == "613347276647039016");
-      client.guilds.get("610434388777369602").member.roles.add(verifiedRole)
+      client.guilds.get("610434388777369602").message.author.roles.add(verifiedRole)
     } else {
       let invite = await client.guilds.get("610434388777369602").channels.find('id', '646418925986250762').createInvite({
           maxAge: 0, //maximum time for the invite, in milliseconds
@@ -22,8 +21,8 @@ exports.run = async (client, message, args) => {
         .setColor('#FF470F')
         .setTitle('Wrong Verification Code!')
         .setDescription(`Please, retry the verification process by rejoining the server using this invite: ${invite}`)
-        .addField('Correct Code:', `hm ${code}`, false)
-        .addField('Input:', `hm ${reason}`, false)
+        .addField('**Correct Code:**', `hm ${code}`, false)
+        .addField('**Input:**', `hm ${reason}`, false)
         .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
       member.send(verifyEmbed);
       client.guilds.get("610434388777369602").member.kick('Sent a wrong verification code.')
