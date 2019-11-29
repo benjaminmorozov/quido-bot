719b28003eee90ba1f2cc0fb05aaad6072c02f8e
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-29 23:22:16 +0100

    test

--- index.js
+++ index.js
@@ -66,6 +66,32 @@ client.on('guildMemberAdd', member => {
   	.addField('**Code:**', code, false)
   	.setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
   member.send(joinverifyEmbed);
+  const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 3600000 });
+  collector.on('collect', message => {
+      if (message.content == code) {
+        const verifyEmbed = new Discord.RichEmbed()
+          .setColor('#00D166')
+          .setTitle('Verified!')
+          .setDescription(`Thanks for verifying. Have a nice experience!`)
+          .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
+        member.send(verifyEmbed);
+        let verifiedRole = message.guild.roles.find(role => role.id == "613347276647039016");
+        client.guilds.get("610434388777369602").member.roles.add(verifiedRole)
+      } else {
+        let invite = await message.channel.createInvite({
+            maxAge: 0, //maximum time for the invite, in milliseconds
+            maxUses: 1 //maximum times it can be used
+          }, .catch(console.log);
+        const verifyEmbed = new Discord.RichEmbed()
+          .setColor('#FF470F')
+          .setTitle('Wrong Verification Code!')
+          .setDescription(`Please, retry the verification process by rejoining the server using this invite: ${invite}`)
+          .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
+        member.send(verifyEmbed);
+        client.guilds.get("610434388777369602").member.kick('Sent a wrong verification code.')
+      }
+  });
+  member.kick('Didn\'t complete the verification process in time.')
 });
 
 
