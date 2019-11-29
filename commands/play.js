a6225b725dbaff14b305e2c2ef69c6e56930e024
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-29 17:41:05 +0100

    test

--- commands/play.js
+++ commands/play.js
@@ -1,50 +1,76 @@
-// Put the Music module in the new Client object.
-// This allows for easy access to all the modules
-// functions and data.
-client.music = require("discord.js-musicbot-addon");
+const { Client, Util } = require('discord.js');
+const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
+const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
 
-// Now we start the music module.
-client.music.start(client, {
-  // Set the api key used for YouTube.
-  youtubeKey: "AIzaSyBqox6JvdyuGF7218IoUVtgA5bYe_q_msg",
+const youtube = new YouTube(GOOGLE_API_KEY);
 
-  // Make it so anyone in the voice channel can skip the
-  // currently playing song.
-  anyoneCanSkip: true,
+const queue = new Map();
 
-  // Make it so the owner (you) bypass permissions for music.
-  ownerOverMember: true,
+client.on('warn', console.warn);
 
-  // The cooldown Object.
-  cooldown: {
-    // This disables the cooldown. Not recommended.
-    enabled: false
+client.on('error', console.error);
+
+client.on('ready', () => console.log('Yo this ready!'));
+
+client.on('disconnect', () => console.log('I just disconnected, making sure you know, I will reconnect now...'));
+
+client.on('reconnecting', () => console.log('I am reconnecting now!'));
+
+exports.run = async (client, message) => {
+  const args = msg.content.split(' ');
+  const searchString = args.slice(1).join(' ');
+  const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
+  const serverQueue = queue.get(msg.guild.id);
+
+  const voiceChannel = msg.member.voiceChannel;
+  if (!voiceChannel) return msg.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
+  const permissions = voiceChannel.permissionsFor(msg.client.user);
+  if (!permissions.has('CONNECT')) {
+    return msg.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
   }
-});
-exports.run = async (client, message, args) => {
-  if (msg.author.bot) return; // Good practice to do this.
+  if (!permissions.has('SPEAK')) {
+    return msg.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
+  }
 
-  // I set the Client to this just for ease.
-  // You'll probably have access to it another
-  // way, but this still works.
-  const client = msg.client;
-
-  // Get the command from the message.
-  const command = message.substring(musicbot.botPrefix.length).split(/[ \n]/)[0].trim();
-
-  // Get the suffix, the String after the command.
-  const suffix = message.substring(musicbot.botPrefix.length + command.length).trim();
-
-  // Set the prefix to "!". This is a horrible way to set
-  // one, but it will do for now.
-  let prefix = "!"
-
-  // Now we check if the message starts with the prefix,
-  // and asks for the PLAY command.
-  if (msg.content.startsWith(prefix) && command == "play") {
-    // Now we pass the Message Object (msg) and
-    // the suffix. It will then proceed as it would
-    // with the bot normally.
-    client.music.bot.playFunction(msg, suffix);
-  };
+  if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+    const playlist = await youtube.getPlaylist(url);
+    const videos = await playlist.getVideos();
+    for (const video of Object.values(videos)) {
+      const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
+      await handleVideo(video2, msg, voiceChannel, true); // eslint-disable-line no-await-in-loop
+    }
+    return msg.channel.send(`✅ Playlist: **${playlist.title}** has been added to the queue!`);
+  } else {
+    try {
+      var video = await youtube.getVideo(url);
+    } catch (error) {
+      try {
+        var videos = await youtube.searchVideos(searchString, 10);
+        let index = 0;
+        msg.channel.send(`
+__**Song selection:**__
+${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
+Please provide a value to select one of the search results ranging from 1-10.
+        `);
+        // eslint-disable-next-line max-depth
+        try {
+          var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
+            maxMatches: 1,
+            time: 10000,
+            errors: ['time']
+          });
+        } catch (err) {
+          console.error(err);
+          return msg.channel.send('No or invalid value entered, cancelling video selection.');
+        }
+        const videoIndex = parseInt(response.first().content);
+        var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+      } catch (err) {
+        console.error(err);
+        return msg.channel.send('🆘 I could not obtain any search results.');
+      }
+    }
+    return handleVideo(video, msg, voiceChannel);
+  }
 };
