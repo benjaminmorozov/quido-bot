588ffe4f6d08262a457c23bcfbaeb4d9b7832066
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-29 23:49:57 +0100

    test

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ client.on('guildMemberAdd', member => {
     // A real basic message with the information we need.
     logChannel.send(`${member} **joined**; Invited by **${inviter.username}** (**${invite.uses}** invites).`);
   });
-  let code = makeid(5);
+  const code = makeid(5);
   const joinverifyEmbed = new Discord.RichEmbed()
   	.setColor('#117EA6')
   	.setTitle('Welcome to Quido\'s Club!')
