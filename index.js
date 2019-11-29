3bc1531794c3fbce95b65a7e3a8ea808b154144c
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-29 22:18:21 +0100

    test

--- index.js
+++ index.js
@@ -14,15 +14,6 @@ client.on('ready', () => {
   client.user.setPresence({ game: { name: "Quido's Club > All", type: 0 } });
 });
 
-const completemsg = `Thank you for agreeing to the rules and code of conduct! You are now a verified member of the guild! \nFeel free to choose what roles you’d like, introduce yourself or check out a our other channels. \n\n**Your unique token is your signature that you have read and understood our rules.**\n`
-
-const shortcode = (n) => {
-    const possible = 'ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghjklmnopqrstuvwxyz0123456789'
-    let text = ''
-    for (var i = 0; i < n + 1; i++) text += possible.charAt(Math.floor(Math.random() * possible.length))
-    return text;
-}
-
 // Initialize the invite cache
 const invites = {};
 
@@ -58,36 +49,6 @@ client.on('guildMemberAdd', member => {
     logChannel.send(`${member} **joined**; Invited by **${inviter.username}** (${invite.uses} invites).`);
   });
 });
-
-client.on('guildMemberAdd', (member) => {
-    if (member.user.bot || member.guild.id !== config.guild) return
-    const token = shortcode(8)
-    const welcomemsg = `Welcome to the guild! We hope you find a home here! Check out the \`#general\` channel to make sure that we jive, and as long as our goals are similar, then there’s a place at the table waiting for you. \n\n If you accept the code of conduct, please verify your agreement by replying to **this DM** with the verification phrase: \n\n\`I agree to abide by all rules. My token is ${token}.\`\n\n **This message is case-sensitive, and please include the period at the end! ** \n\nQuestions? Get at a staff member in the server or via DM.`
-    console.log(`${member.user.username}#${member.user.discriminator} joined! CODE: "${token}"`)
-    member.send(welcomemsg)
-    member.user.token = token
-})
-
-const verifymsg = 'I agree to abide by all rules. My token is {token}.'
-
-client.on('message', (message) => {
-    if (message.author.bot || !message.author.token || message.channel.type !== `dm`) return
-    if (message.content !== (verifymsg.replace('{token}', message.author.token))) return
-    message.channel.send({
-        embed: {
-            color: Math.floor(Math.random() * (0xFFFFFF + 1)),
-            description: completemsg,
-            timestamp: new Date(),
-            footer: {
-                text: `Verification Success`
-            }
-        }
-    })
-    let verifiedRole = message.guild.roles.find(role => role.name == "Member");
-    client.guilds.get("610434388777369602").member(message.author).roles.add(verifiedRole)
-        .then(console.log(`TOKEN: ${message.author.token} :: Role ${config.role} added to member ${message.author.id}`))
-        .catch(console.error)
-})
 
 client.on('message', (message) => {
     if (message.author.bot || !message.author.token || message.channel.type !== `dm`) return
