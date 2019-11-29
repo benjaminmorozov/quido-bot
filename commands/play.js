30ce6d2312157a370203b117380e94e9ad929a00
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-29 17:35:05 +0100

    test

--- commands/play.js
+++ commands/play.js
@@ -1,25 +1,50 @@
-const ytdl = require('ytdl-core');
+// Put the Music module in the new Client object.
+// This allows for easy access to all the modules
+// functions and data.
+client.music = require("discord.js-musicbot-addon");
 
+// Now we start the music module.
+client.music.start(client, {
+  // Set the api key used for YouTube.
+  youtubeKey: "AIzaSyBqox6JvdyuGF7218IoUVtgA5bYe_q_msg",
+
+  // Make it so anyone in the voice channel can skip the
+  // currently playing song.
+  anyoneCanSkip: true,
+
+  // Make it so the owner (you) bypass permissions for music.
+  ownerOverMember: true,
+
+  // The cooldown Object.
+  cooldown: {
+    // This disables the cooldown. Not recommended.
+    enabled: false
+  }
+});
 exports.run = async (client, message, args) => {
-  if(!message.member.voiceChannel)
-    return message.channel.send('Please, connect to a voice channel.');
+  if (msg.author.bot) return; // Good practice to do this.
 
-  if(message.guild.me.voiceChannel)
-    return message.channel.send('Sorry, the bot is already connected to a voice channel.');
+  // I set the Client to this just for ease.
+  // You'll probably have access to it another
+  // way, but this still works.
+  const client = msg.client;
 
-  if(!args[0])
-    return message.channel.send('Sorry, please input a url following the command.');
+  // Get the command from the message.
+  const command = message.substring(musicbot.botPrefix.length).split(/[ \n]/)[0].trim();
 
-  let validate = await ytdl.validateURL(args[0]);
+  // Get the suffix, the String after the command.
+  const suffix = message.substring(musicbot.botPrefix.length + command.length).trim();
 
-  if(!validate)
-    return message.channel.send('Sorry, please input a **valid** url following the command.');
+  // Set the prefix to "!". This is a horrible way to set
+  // one, but it will do for now.
+  let prefix = "!"
 
-  let info = await ytdl.getInfo(args[0]);
-
-  let connection = await message.member.voiceChannel.join();
-
-  let dispatcher = await connection.play(ytdl(args[0], {filter: 'audioonky'}));
-
-  message.channel.send(`Now playing: ${info.title}`);
+  // Now we check if the message starts with the prefix,
+  // and asks for the PLAY command.
+  if (msg.content.startsWith(prefix) && command == "play") {
+    // Now we pass the Message Object (msg) and
+    // the suffix. It will then proceed as it would
+    // with the bot normally.
+    client.music.bot.playFunction(msg, suffix);
+  };
 };
