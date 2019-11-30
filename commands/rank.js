3382f879f02b49afd1819c8e9175c41b7e5b9574
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-30 15:26:34 +0100

    test

--- commands/rank.js
+++ commands/rank.js
@@ -1,11 +1,3 @@
-const { Canvas } = require('canvas-constructor');
-const { Attachment } = require('discord.js');
-const { resolve, join } = require('path');
-const fetch = require('node-fetch');
-
-const imageUrlRegex = /\?size=2048$/g;
-const placeholder = new Map();
-
 const Discord = require("discord.js");
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://admin:admin@quido-bot-sku03.mongodb.net/test?retryWrites=true&w=majority", {
@@ -15,52 +7,18 @@ const Score = require("../models/score.js")
 
 exports.run = async (client, message, args) => {
   Score.findOne({userID: message.author.id, serverID: message.guild.id}, (err, score) => {
-    const buffer = await profile(message, score.score);
-    const filename = `profile-${message.author.id}.jpg`;
-    const attachment = new Attachment(buffer, filename);
-    await message.channel.send(attachment);
+    if(err) console.log(err);
+
+    let embed = new Discord.RichEmbed()
+    .setTitle("Score")
+    .setColor("#117EA6")
+    .setThumbnail(message.author.displayAvatarURL);
+    if(!score){
+      embed.addField("Score", "0", true);
+      return message.channel.send(embed);
+    } else {
+      embed.addField("Score", score.score, true);
+      return message.channel.send(embed);
+    }
   })
 };
-
-const profile = async function(message, score) {
-  Score.findOne({userID: message.author.id, serverID: message.guild.id}, (err, score) => {
-  const member = message.member;
-
-  try {
-    const result = await fetch(member.user.displayAvatarURL.replace(imageUrlRegex, '?size=128'));
-    if (!result.ok) throw new Error('Failed to get the avatar!');
-    const avatar = await result.buffer();
-
-    const name = member.displayName.length > 30 ? member.displayName.substring(0, 17) + '...'
-      : member.displayName;
-
-    return new Canvas(400, 180)
-      .setColor('#7289DA')
-      .addRect(84, 0, 316, 180)
-      .setColor("#2C2F33")
-      .addRect(0, 0, 84, 180)
-      .addRect(169, 26, 231, 46)
-      .addRect(224, 108, 176, 46)
-      .setShadowColor('rgba(22, 22, 22, 1)')
-      .setShadowOffsetY(5)
-      .setShadowBlur(10)
-      .addCircle(84, 90, 62)
-      .addCircularImage(avatar, 85, 90, 64)
-      .save()
-      .createBeveledClip(20, 138, 128, 32, 5)
-      .setColor('#23272A')
-      .fill()
-      .restore()
-      .setTextAlign('center')
-      .setTextFont('18pt Klavika Regular')
-      .setColor('#FFFFFF')
-      .addText(name, 285, 54)
-      .addText(`Level: test`, 84, 159)
-      .setTextAlign('left')
-      .addText(`Score: ${score.score}`, 241, 136)
-      .toBuffer();
-  } catch (error) {
-    await message.channel.send(`An error occurred: **${error.message}**`);
-  }
-});
-}
