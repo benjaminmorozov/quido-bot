2ecb957c19e5271161bc9712cec7335363e6a730
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-30 15:07:46 +0100

    test

--- commands/rank.js
+++ commands/rank.js
@@ -1,3 +1,4 @@
+Canvas.registerFont(resolve(join(__dirname, "../assets/Discord.ttf")), "Discord");
 const { Canvas } = require("canvas-constructor"); // You can't make images without this.
 const { resolve, join } = require("path"); // This is to get a font file.
 const { Attachment } = require("discord.js"); // This is to send the image via discord.
@@ -13,20 +14,6 @@ const imageUrlRegex = /\?size=2048$/g;
 
 exports.run = async (client, message, args) => {
   Score.findOne({userID: message.author.id, serverID: message.guild.id}, (err, score) => {
-    if(err) console.log(err);
-
-    let embed = new Discord.RichEmbed()
-    .setTitle("Score")
-    .setColor("#117EA6")
-    .setThumbnail(message.author.displayAvatarURL);
-    if(!score){
-      embed.addField("Score", "0", true);
-      return message.channel.send(embed);
-    } else {
-      embed.addField("Score", score.score, true);
-      return message.channel.send(embed);
-    }
-  });
   async function profile(member, score) {
     try {
       const result = await fetch(member.user.displayAvatarURL.replace(imageUrlRegex, "?size=128"));
@@ -39,11 +26,54 @@ exports.run = async (client, message, args) => {
       const name = member.displayName.length > 20 ? member.displayName.substring(0, 17) + "..." : member.displayName;
 
       // ...
+    } catch (error) {
+      await message.channel.send(`Something happened: ${error.message}`);
     };
     return new Canvas(400, 180)
+      // Create the Blurple rectangle on the right side of the image.
+      .setColor("#7289DA")
+      .addRect(84, 0, 316, 180)
+      // Create the "Dark, but not black" boxes for the left side of the image
+      // and the text boxes on the right.
+      .setColor("#2C2F33")
+      .addRect(0, 0, 84, 180)
+      .addRect(169, 26, 231, 46)
+      .addRect(224, 108, 176, 46)
+      // Create a shadow effect for the avatar placement.
+      .setShadowColor("rgba(22, 22, 22, 1)") // This is a nice colour for a shadow.
+      .setShadowOffsetY(5) // Drop the shadow by 5 pixels.
+      .setShadowBlur(10) // Blur the shadow by 10.
+      // This circle is 2 pixels smaller in the radius to prevent a pixel border.
+      .addCircle(84, 90, 62)
+      .addCircularImage(avatar, 20, 26, 64)
+      // This creates a rounded corner rectangle, you must use save and restore to
+      // clear the clip after we are done with it
+      .save()
+      .createBeveledClip(20, 138, 128, 32, 5)
+      .setColor("#23272A")
+      .fill()
+      .restore()
+      // Add all of the text for the template.
+      // Let's center the text
+      .setTextAlign("center")
+      // I'm using a custom font, which I will show you how to add next.
+      .setTextFont("10pt Discord")
+      // Set the colour to white, since we have a dark background for all the text boxes.
+      .setColor("#FFFFFF")
+      // Add the name variable.
+      .addText(name, 285, 54)
+      // Using template literals, you can add text and variables, we're applying the toLocaleString()
+      // to break up the number into a nice readable format.
+      .addText(`Level: test`, 84, 159)
+      // Now we want to align the text to the left.
+      .setTextAlign("left")
+      // Let's add all the points!
+      .addText(`Score: ${score}`, 241, 136)
+      .toBuffer()
   }
   const buffer = await profile(message.member, score.score);
   const filename = `profile-${message.author.id}.jpg`;
   const attachment = new Attachment(buffer, filename);
   await message.channel.send(attachment);
+  });
 };
