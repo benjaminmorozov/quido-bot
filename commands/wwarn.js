5109d005a219b4ea5e85c294799101a106f01b60
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-11-30 17:03:36 +0100

    test

--- commands/wwarn.js
+++
@@ -1,9 +0,0 @@
-const Discord = require("discord.js");
-const Enmap = require("enmap");
-const mongoose = require("mongoose");
-mongoose.connect('mongodb+srv://quido-bot-sku03.mongodb.net/test');
-
-exports.run = async (client, message, args) => {
-    if(!message.member.roles.some(r=>["🔱OWNER🔱","Discord Manager & Designer","Administrator", "Moderator","Head Admin","Admin","Helper"].includes(r.name)) )
-    return message.reply("sorry, you don't have enough permissions to use this!");
-};
