81af7c2b90e6a8a2a7b212fdbfc6a87da956c22a
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-12-03 23:02:01 +0100

    test

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,15 +2,11 @@ const Discord = require("discord.js");
 const moment = require("moment");
 exports.run = (client, message, args) => {
     const member = message.mentions.users.first() || message.author;
-    if(member.avatarURL === 'null') {
-      let avatarURL = 'https://i.pinimg.com/originals/b8/5d/0b/b85d0b60c0283fe7fd3f34cd0df87f15.png';
-    } else {
-      let avatarURL = member.avatarURL;
-    }
+    if(member.avatarURL === 'null') member.avatarURL = "https://i.pinimg.com/originals/b8/5d/0b/b85d0b60c0283fe7fd3f34cd0df87f15.png";
     const userinfoEmbed= new Discord.RichEmbed()
       .setColor('0xff5353')
       .setAuthor(`${member.username}`)
-      .setThumbnail(`${avatarURL}`)
+      .setThumbnail(`${member.avatarURL}`)
       .addField('**Full Username:**', `${member.username}#${member.discriminator}`, false)
       .addField('**Member ID:**', `${member.id}`, false)
       .addField('**Account Creation Date:**', `${moment.utc(member.createdAt).format('dddd DD/MM/YYYY')}`, false)
