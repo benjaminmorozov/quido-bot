3dff5588cab1ba8da8db27e8828c168fb69f5e3a
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-12-03 22:58:19 +0100

    test

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const moment = require("moment");
 exports.run = (client, message, args) => {
     const member = message.mentions.users.first() || message.author;
-    if(member.avatarURL === 'null') let member.avatarURL = "https://i.pinimg.com/originals/b8/5d/0b/b85d0b60c0283fe7fd3f34cd0df87f15.png";
+    if(member.avatarURL === 'null')
+      let member.avatarURL = 'https://i.pinimg.com/originals/b8/5d/0b/b85d0b60c0283fe7fd3f34cd0df87f15.png';
     const userinfoEmbed= new Discord.RichEmbed()
       .setColor('0xff5353')
       .setAuthor(`${member.username}`)
