359d73ba3688bd1f69edcd6f348417f40a4e54cb
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-12-03 22:39:49 +0100

    test

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,15 +1,14 @@
 const moment = require("moment");
 exports.run = (client, message, args) => {
     const member = message.mentions.users.first() || message.author;
-      const userinfoEmbed= new Discord.RichEmbed()
-      	.setColor('0xff5353')
-      	.setAuthor(`${member.username}`)
-      	.setThumbnail(`${member.avatarURL}`)
-      	.addField('**Full Username:**', `${member.username}#${member.discriminator}`, false)
-        .addField('**Member ID:**', `${member.id}`, false)
-        .addField('**Account Creation Date:**', `${moment.utc(member.createdAt).format('dddd DD/MM/YYYY')}`, false)
-        .addField('**Member Join Date:**', `${moment.utc(member.JoinedAt).format('dddd DD/MM/YYYY')}`, false)
-        .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
-      message.channel.send(userinfoEmbed);
-    };
-}
+    const userinfoEmbed= new Discord.RichEmbed()
+      .setColor('0xff5353')
+      .setAuthor(`${member.username}`)
+      .setThumbnail(`${member.avatarURL}`)
+      .addField('**Full Username:**', `${member.username}#${member.discriminator}`, false)
+      .addField('**Member ID:**', `${member.id}`, false)
+      .addField('**Account Creation Date:**', `${moment.utc(member.createdAt).format('dddd DD/MM/YYYY')}`, false)
+      .addField('**Member Join Date:**', `${moment.utc(member.JoinedAt).format('dddd DD/MM/YYYY')}`, false)
+      .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
+    message.channel.send(userinfoEmbed);
+};
