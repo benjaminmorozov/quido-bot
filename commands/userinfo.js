055d5a0e6f2bb12e4f4e68025101344092bc963b
Benjamín Morozov <benjaminmorozov@gmail.com>
2019-12-03 22:35:43 +0100

    test

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,40 +1,15 @@
 const moment = require("moment");
 exports.run = (client, message, args) => {
     const member = message.mentions.users.first() || message.author;
-
-    message.channel.send({embed: {
-        color: 0xff5353,
-        author: {
-          name: `${member.username}`,
-        },
-        thumbnail: {
-            "url": `${member.avatarURL}`
-          },
-        fields: [{
-            name: "Full Username:",
-            value: `${member.username}#${member.discriminator}`,
-            inline: "false"
-          },
-          {
-            name: "Member ID:",
-            value: `${member.id}`,
-            inline: "false"
-          },
-          {
-            name: "Account Creation Date:",
-            value: `${moment.utc(member.createdAt).format('dddd DD/MM/YYYY')}`,
-            inline: "false"
-          },
-          {
-            name: "Member Join Date:",
-            value: `${moment.utc(member.JoinedAt).format('dddd DD/MM/YYYY')}`,
-            inline: "false"
-          },
-        ],
-        footer: {
-          icon_url: `${client.user.avatarURL}`,
-          text: "Thanks for being a part of our community ❤"
-        }
-      }
+      const userinfoEmbed= new Discord.RichEmbed()
+      	.setColor('0xff5353')
+      	.setAuthor(`${member.username}`)
+      	.setThumbnail(`${member.avatarURL}`)
+      	.addField('**Full Username:**', `${member.username}#${member.discriminator}`, false)
+        .addField('**Member ID:**', `${member.id}`, false)
+        .addField('**Account Creation Date:**', `${moment.utc(member.createdAt).format('dddd DD/MM/YYYY')}`, false)
+        .addField('**Member Join Date:**', `${moment.utc(member.JoinedAt).format('dddd DD/MM/YYYY')}`, false)
+        .setFooter('Thanks for being a part of our community. ❤️', `${client.user.avatarURL}`);
+      message.channel.send(userinfoEmbed);
     });
 }
