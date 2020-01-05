dae4b020d58018e41ef0fc455ea5f0ea2cb13533
Benjamín <benjaminmorozov@gmail.com>
2020-01-05 16:36:11 +0100

    test

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,14 +2,6 @@ const Discord = require("discord.js");
 const moment = require("moment");
 exports.run = (client, message, args) => {
     const member = message.mentions.users.first() || message.author;
-	let joinDate = message.guild.member(member).joinedAt.toUTCString().substr(0, 16);
-	var date = new Date(joinDate);
-	var currentDate = new Date();
-	if(date >= currentDate - 1 month) {
-		let newJoinDate = `**⚠ ${joinDate}**`
-	else {
-		let newJoinDate = `${joinDate}`
-	};
     var avatar = member.avatarURL;
     if(avatar == null) avatar = "https://discordapp.com/assets/0e291f67c9274a1abdddeb3fd919cbaa.png";
     const userinfoEmbed= new Discord.RichEmbed()
@@ -19,7 +11,7 @@ exports.run = (client, message, args) => {
       .addField('**Full Username:**', member.username + `#` + member.discriminator, false)
       .addField('**Member ID:**', member.id, false)
       .addField('**Account Creation Date:**', `${moment.utc(member.createdAt).format('dddd DD/MM/YYYY')}`, false)
-      .addField('**Member Join Date:**', `${newJoinDate}`, false)
+      .addField('**Member Join Date:**', `${moment.utc(message.guild.member(member).joinedAt).format(('dddd DD/MM/YYYY')}`, false)
       .setFooter('Thanks for being a part of our community. ❤️', message.guild.iconURL);
     message.channel.send(userinfoEmbed);
 };
