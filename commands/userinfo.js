122bd8c4e5abdc4674e582c47020f4a240c6c44a
Benjamín <benjaminmorozov@gmail.com>
2020-01-05 19:48:15 +0100

    test

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,11 +2,10 @@ const Discord = require("discord.js");
 const moment = require("moment");
 exports.run = (client, message, args) => {
     const member = message.mentions.users.first() || message.author;
-	let d = member.createdTimestamp;
-			var OneDay = new Date().getTime() + (7 * 1 * 24 * 60 * 60 * 1000)
 	function creationDate() {
-										 // week day hour min  sec  msec
-		if (OneDay > d) return `**⚠️ ${moment.utc(member.createdAt).format('dddd DD/MM/YYYY')} ⚠**`; // danger if age less than 1 month
+		let d = member.createdTimestamp;
+		var ONE_MONTH = 31 * 24 * 60 * 60 * 1000; /* ms */
+		if(((new Date) - d) < ONE_MONTH) return `**⚠️ ${moment.utc(member.createdAt).format('dddd DD/MM/YYYY')} ⚠**`; // danger if age less than 1 month
 		return moment.utc(member.createdAt).format('dddd DD/MM/YYYY');  // Looks good!
 	}
     var avatar = member.avatarURL;
@@ -17,7 +16,7 @@ exports.run = (client, message, args) => {
       .setThumbnail(member.displayAvatarURL)
       .addField('**Full Username:**', member.username + `#` + member.discriminator, false)
       .addField('**Member ID:**', member.id, false)
-      .addField('**Account Creation Date:**', `${creationDate()} (${d} + ${OneDay})`, false)
+      .addField('**Account Creation Date:**', `${creationDate()}`, false)
       .addField('**Member Join Date:**', `${moment.utc(message.guild.member(member).joinedAt).format('dddd DD/MM/YYYY')}`, false)
       .setFooter('Thanks for being a part of our community. ❤️', message.guild.iconURL);
     message.channel.send(userinfoEmbed);
