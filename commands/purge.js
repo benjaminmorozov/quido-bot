a9525b2f8868f53518c86a4ee0bdfcb295c0c82e
Benjamín <benjaminmorozov@gmail.com>
2020-01-05 17:29:51 +0100

    test

--- commands/purge.js
+++ commands/purge.js
@@ -3,6 +3,7 @@ exports.run = async (client, message, args) => {
     return message.reply("you don't have enough permissions to execute this command!");
     // makes the bot say something and delete the message. As an example, it's open to anyone to use.
     // To get the "message" itself we join the `args` back into a string with spaces:
+	message.delete();
     if (!args.length) {
         return;
     }
@@ -14,7 +15,6 @@ exports.run = async (client, message, args) => {
       return message.reply("provide a number between 2 and 100 for the number of messages to delete.");
 
     // So we get our messages, and delete them. Simple enough, right?
-	message.delete();
     const fetched = await message.channel.fetchMessages({limit: deleteCount});
     message.channel.bulkDelete(fetched)
       .catch(error => message.reply(`couldn't delete messages because of: ${error}`));
--- commands/userinfo.js
+++ commands/userinfo.js
@@ -4,9 +4,8 @@ exports.run = (client, message, args) => {
     const member = message.mentions.users.first() || message.author;
 	function creationDate() {
 		var d = message.guild.member(member).createdAt;
-		var today = new Date();
-		let diff = today.diff(d, 'months');
-		if(diff < 1) return `**⚠️ ${moment.utc(member.createdAt).format('dddd DD/MM/YYYY')} ⚠**`; // danger if age less than 1 month
+		var ONE_MONTH = 31 * 24 * 60 * 60 * 1000; /* ms */
+		if(((new Date) - d) < ONE_MONTH) return `**⚠️ ${moment.utc(member.createdAt).format('dddd DD/MM/YYYY')} ⚠**`; // danger if age less than 1 month
 		return moment.utc(member.createdAt).format('dddd DD/MM/YYYY');  // Looks good!
 	}
     var avatar = member.avatarURL;
