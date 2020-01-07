852875b356e58a61fe11184053aaf2d0aafdca4a
Benjamín <benjaminmorozov@gmail.com>
2020-01-07 09:07:00 +0100

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
+const moment = require("moment");
 const { RichEmbed } = require('discord.js');
 const client = new Discord.Client();
 const getImages = require('./util/getImages');
