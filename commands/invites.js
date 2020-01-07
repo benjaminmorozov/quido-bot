fa883a67798756859a83248b96ffb1f8a55b1a2d
Benjamín <benjaminmorozov@gmail.com>
2020-01-07 09:42:24 +0100

    test

---
+++ commands/invites.js
@@ -0,0 +1,22 @@
+exports.run = (client, message) => {
+    const targetUser = message.mentions.users.first() || message.author;
+
+    message.guild.fetchInvites()
+	.then
+	(invites =>
+		{
+			const userInvites = invites.array().filter(o => o.inviter.id === targetUser.id);
+			var userInviteCount = 0;
+				for(var i=0; i < userInvites.length; i++)
+				{
+					var invite = userInvites[i];
+					userInviteCount += invite['uses'];
+				}
+			if(!targetUser.id == message.author.id)
+				message.channel.send(`User _${targetUser.username}_ has invited ${userInviteCount} user(s) to this server.`);
+			else
+				message.reply(`You have invited ${userInviteCount} user(s) to this server. Keep up the good work!`);
+		}
+    )
+    .catch(console.error);
+}
