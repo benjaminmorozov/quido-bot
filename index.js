bc06fcf5429acb72658b18150787f2ad1a65677d
Benjamín <benjaminmorozov@gmail.com>
2020-01-10 19:56:57 +0100

    test

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ const Score = require("./models/score.js");
 const Verify = require("./models/verify.js");
 
 const config = require("./config.json");
+const words = rrequire("./words.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
+client.words = words;
 
 client.on('ready', () => {
   console.log(`Successfully loaded and logged in as ${client.user.tag}.`);
-  client.user.setPresence({ game: { name: "Welcome to Quido\'s Club ️️", type: 0 } });
+  client.user.setPresence({ game: { name: 'Welcome to Quido\'s Club ️❤️', type: 0 } });
 });
 
 function makeid(length) {
@@ -48,6 +50,11 @@ client.on('ready', () => {
     });
   });
 });
+
+client.on('message', message => {
+  if(words.filter-words.some(word => message.content.toLowerCase().includes(word))){
+    message.delete()
+}});
 
 client.on('guildMemberAdd', member => {
   global.verifymember = member;
