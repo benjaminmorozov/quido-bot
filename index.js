669abc0730f4cb291d843faafa18135ca48eedde
Benjamín <benjaminmorozov@gmail.com>
2020-01-10 19:58:09 +0100

    test

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const Score = require("./models/score.js");
 const Verify = require("./models/verify.js");
 
 const config = require("./config.json");
-const words = rrequire("./words.json");
+const words = require("./words.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 client.words = words;
